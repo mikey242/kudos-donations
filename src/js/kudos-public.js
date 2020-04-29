21bf8996f3856fe24627a3c22f831ee21d551d31
Michael Iseard <michael@iseard.media>
2020-04-29 22:18:50 +0200

    Refactor modals to separate file

--- src/js/kudos-public.js
+++ src/js/kudos-public.js
@@ -1,48 +1,27 @@
 import $ from 'jquery';
-import KudosModal from "./kudos-modals";
+import {init as initModal, messageModal, donateModal} from "./kudos-modals";
 import "jquery-validation";
 import Mustache from "mustache";
 import MicroModal from "micromodal";
 import {library, dom} from "@fortawesome/fontawesome-svg-core";
 import {faLock, faCircle} from "@fortawesome/free-solid-svg-icons";
-import logo from "../img/logo-colour.svg";
 
 library.add(faLock, faCircle);
 dom.watch();
 
-$(function () {
-
-    // console.log(modalFrame());
+$(() => {
 
     const $body = $('body');
     let $kudosButtons = $('.kudos_btn');
+    let $kudosModal;
     let redirectUrl;
     let order_id = new URLSearchParams(location.search).get('kudos_order_id');
 
-    // Add kudos_mollie class to body if button found
+    // Add kudos_mollie class and modal markup to body if button found
     if($kudosButtons.length) {
         $body.addClass('kudos_mollie');
+        $kudosModal = initModal();
     }
-
-    let modalLogo = new Image(40);
-    modalLogo.src = logo;
-
-    let $kudosModal = $('\
-        <div id="kudos_modal" class="kudos_modal" aria-hidden="true">\
-            <div class="kudos_modal_overlay flex justify-center items-center fixed top-0 left-0 w-full h-full bg-modal z-50" tabindex="-1" data-micromodal-close>\
-                <div class="kudos_modal_container bg-white py-4 px-8 rounded-lg max-h-screen max-w-lg relative overflow-hidden" role="dialog" aria-modal="true" aria-labelledby="kudos_modal-title">\
-                    <header class="kudos_modal_header flex items-center justify-between">\
-                        <div class="kudos_modal_title mt-0 mb-0" id="kudos_modal-title">\
-                        '+modalLogo.outerHTML+'\
-                        </div>\
-                        <button class="kudos_modal_close text-black p-0" aria-hidden="true" aria-label="Close modal" data-micromodal-close></button>\
-                    </header>\
-                    <main class="kudos_modal_content" id="kudos_modal_content"></main>\
-                </div>\
-            </div>\
-        </div>\
-    ');
-
 
     // Check order status if query var exists
     if(order_id) {
@@ -55,7 +34,6 @@ $(function () {
                 order_id: order_id
             },
             success: function (result) {
-                console.log(result);
                 if(result.success) {
                     let $content = '';
                     let message;
@@ -64,17 +42,9 @@ $(function () {
                         case 'paid':
                         case 'open':
                             message = Mustache.render(data.modalText, {value: Math.round(data.transaction.value)});
-                            $content = $('\
-                                <div class="top-content text-center">\
-                                    <h2 class="font-normal">' + data.modalHeader + '</h2><p>'+ message +'</p>\
-                                </div>\
-                                <footer class="kudos_modal_footer mt-4 text-right">\
-                                    <button class="kudos_btn kudos_btn_primary" type="button" data-micromodal-close aria-label="Close this dialog window">Ok</button>\
-                                </footer>\
-                            ');
                             break;
                     }
-                    $body.append($kudosModal);
+                    $content = messageModal(data.modalHeader, message);
                     MicroModal.show('kudos_modal', {
                         onShow: modal => $(modal).find('#kudos_modal_content').html($content),
                         awaitCloseAnimation: true
@@ -95,33 +65,10 @@ $(function () {
             let customHeader = $(this).data('customHeader');
             let customText = $(this).data('customText');
 
-            let content = $('\
-                <div class="top-content text-center">\
-                    <h2 class="font-normal">'+ customHeader +'</h2>\
-                    <p>'+ customText +'</p>\
-                </div>\
-                <form id="kudos_form" action="">\
-                    <input type="text" name="name" placeholder="Naam (optioneel)" />\
-                    <input type="email" class="mt-3" name="email_address" placeholder="E-mailadres (optioneel)" />\
-                    <input required type="text" min="1" class="mt-3" name="value" placeholder="Bedrag (in euro\'s) *" />\
-                    <div class="payment_by mt-3 text-muted text-right"><small class="text-gray-600">\
-                        <span class="fa-stack fa-xs align-middle">\
-                            <i class="fas fa-circle fa-stack-2x"></i>\
-                            <i class="fas fa-lock fa-stack-1x fa-inverse"></i>\
-                        </span>\
-                        Beveiligde betaling via\
-                    </small></div>\
-                    <footer class="kudos_modal_footer mt-4 text-center">\
-                        <button class="kudos_btn kudos_btn_primary_outline mr-3" type="button" data-micromodal-close aria-label="Close this dialog window">Annuleren</button>\
-                        <button id="kudos_submit" class="kudos_btn kudos_btn_primary" type="submit">Doneeren</button>\
-                    </footer>\
-                </form>\
-                <i class="kudos_spinner"></i> \
-            ');
+            let $content = donateModal(customHeader, customText);
 
-            $body.append($kudosModal);
             MicroModal.show('kudos_modal', {
-                onShow: modal => $(modal).find('#kudos_modal_content').html(content),
+                onShow: modal => $(modal).find('#kudos_modal_content').html($content),
                 awaitCloseAnimation: true
             });
         })
