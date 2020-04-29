9a2e5c3cf95dcbcfa7b4160db6340b05677439f7
Michael Iseard <michael@iseard.media>
2020-04-29 11:02:44 +0200

    Use tailwind to configure theme colours

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        orange: {
+          '500': '#ff9f1c',
+          '700': '#f58d00',
+        },
+        green: '#2ec4b6'
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
