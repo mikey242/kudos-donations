b69e7a39e9b72365fd87aff5cae42c9d7aceb476
Michael Iseard <michael@iseard.media>
2020-05-03 00:31:20 +0200

    Refactor to make it easier to use env vars in future

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,19 @@
 /* eslint-disable */
-const purgecss = require('@fullhuman/postcss-purgecss');
 const tailwindcss = require('tailwindcss');
-const cssnano = require('cssnano');
+
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    content: ['./src/js/*.js', './public/**/*.php'],
+    whitelistPatternsChildren: [/kudos/, /orange/]
+});
+
+const cssnano = require('cssnano')({
+    preset: 'default'
+});
 
 module.exports = {
     plugins: [
         require('autoprefixer'),
         tailwindcss('./tailwind.config.js'),
-        cssnano({
-            preset: 'default'
-        }),
-        purgecss({
-            content: ['./src/js/*.js', './public/**/*.php'],
-            whitelistPatternsChildren: [/kudos/, /orange/]
-        })
+        ...[cssnano, purgecss]
     ]
 }
