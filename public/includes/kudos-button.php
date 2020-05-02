83830483f013ca87f5a2b12dc6e94d5b3a9dff68
Michael Iseard <michael@iseard.media>
2020-05-03 00:29:34 +0200

    Rename variables for consistency

--- public/includes/kudos-button.php
+++ public/includes/kudos-button.php
@@ -3,23 +3,24 @@
 use Kudos\Kudos_Public;
 
 /**
- * @param null $label
- * @param null $customText
+ * @param string|null $customLabel
+ * @param string|null $customHeader
+ * @param string|null $customText
  * @param bool $echo
  *
  * @return string
  */
-function kudos_button($label=null, $customHeader=null, $customText=null, $echo=true) {
+function kudos_button($customLabel=null, $customHeader=null, $customText=null, $echo=true) {
 
 	if(Kudos_Public::ready()) {
-		$label = $label ? $label : __('Donate now', 'kudos-donations');
 		$redirectUrl = Kudos_Public::get_return_url();
-		$customText = $customText ? $customText : __("We really appreciate that you want to help us out.", 'kudos-donations');
-		$customHeader = $customHeader ? $customHeader : __("Support us!", 'kudos-donations');
+		$label = $customLabel ? $customLabel : carbon_get_theme_option('kudos_button_label');
+		$text = $customText ? $customText : carbon_get_theme_option('kudos_form_text');
+		$header = $customHeader ? $customHeader : carbon_get_theme_option('kudos_form_header');
 		$style = carbon_get_theme_option('kudos_button_style');
 
-		$out = "<button class='kudos_btn kudos_button_icon $style' data-redirect='$redirectUrl' data-custom-header='$customHeader' data-custom-text='$customText'>";
-		$out .= "<span class='kudos_logo'></span><span class='kudos_block_button_block__label'>$label</span>";
+		$out = "<button class='kudos_btn kudos_button_icon $style' data-redirect='$redirectUrl' data-custom-header='$header' data-custom-text='$text'>";
+			$out .= "<span class='kudos_logo'></span><span class='kudos_block_button_block__label'>$label</span>";
 		$out .= "</button>";
 
 	} elseif(is_user_logged_in()) {
