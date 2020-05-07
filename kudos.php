bcc7aa7e04b72f478f870687ab5e3cf5751755a2
Michael Iseard <michael@iseard.media>
2020-05-07 17:08:22 +0200

    Remove comments

--- kudos.php
+++ kudos.php
@@ -34,16 +34,7 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-/**
- * Current plugin version.
- * Start at version 1.0.0 and use SemVer - https://semver.org
- * Rename this for your plugin and update it as you release new versions.
- */
 define( 'KUDOS_VERSION', '1.0.0' );
-
-/**
- * Plugin directory constant
- */
 define('KUDOS_DIR', plugin_dir_path(__FILE__));
 
 /**
