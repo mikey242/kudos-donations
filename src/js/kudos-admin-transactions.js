76f758ecc57f1dd325c865c35a086a75b013d54c
Michael Iseard <michael@iseardmedia.com>
2020-07-26 15:11:34 +0200

    Remove unused import

--- src/js/kudos-admin-transactions.js
+++ src/js/kudos-admin-transactions.js
@@ -1,4 +1,3 @@
-import $ from 'jquery';
 import { dom, library } from '@fortawesome/fontawesome-svg-core';
 import { faCreditCard } from '@fortawesome/free-solid-svg-icons';
 import { faFilePdf } from '@fortawesome/free-regular-svg-icons';
