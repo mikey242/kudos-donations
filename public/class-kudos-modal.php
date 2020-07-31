714ad4fa3b6ab1fbb5b3063bc3c23b3b7fba5ad0
Michael Iseard <michael@iseardmedia.com>
2020-07-31 14:10:52 +0200

    Remove unnecessary ready property and check

--- public/class-kudos-modal.php
+++ public/class-kudos-modal.php
@@ -33,7 +33,6 @@ class Kudos_Modal {
 	public function __construct() {
 
 	    $this->logger = new Kudos_Logger();
-	    $this->ready = Kudos_Public::ready();
 	    $this->returnUrl = Kudos_Public::get_return_url();
 	    $this->twig = new Kudos_Twig();
 	    $this->id = uniqid('kudos_modal-');
@@ -70,33 +69,33 @@ class Kudos_Modal {
 	 * @return bool
 	 * @since    1.0.0
 	 */
-	function get_payment_modal($atts=[]) {
+	function get_payment_modal($atts) {
 
 		$privacy_option = get_option("_kudos_privacy_link");
 		$privacy_link = __('I agree with the privacy policy.', "kudos-donations");
 		if($privacy_option) {
-			$privacy_link = sprintf(__('I agree with the %s', "kudos-donations"), '<a target="_blank" href='. get_option("_kudos_privacy_link") .'>'.__("privacy policy.", "kudos-donations"). '</a>');
+			$privacy_link = sprintf(__('I agree with the %s', "kudos-donations"), '<a target="_blank" href='. get_option("_kudos_privacy_link") .'>'.__("privacy policy", "kudos-donations"). '</a>.');
 		}
 
-		if($this->ready) {
-		    $data = [
-	            'modal_id' => $this->id,
-	            'header' => !empty($atts['header']) ? $atts['header'] : get_option('_kudos_form_header'),
-                'text' => !empty($atts['text']) ? $atts['text'] : get_option('_kudos_form_text'),
-                'color' => !empty($atts['color']) ? $atts['color'] : get_option('_kudos_button_color'),
-		        'nonce' => wp_nonce_field('kudos_submit', '_wpnonce', true, false),
-	            'address' => [
-		    	    'enabled' => get_option('_kudos_address_enabled'),
-		    	    'required' => get_option('_kudos_address_required')
-				],
-			    'privacy_link' => $privacy_link,
-	            'return_url' => $this->returnUrl,
-			    'donation_label' => $atts['donation_label'],
-		        'payment_by' => __('Secure payment by', 'kudos-donations'),
-		        'vendor' => (get_option('_kudos_payment_vendor') ? get_option('_kudos_payment_vendor') : 'mollie')
-		    ];
-			return $this->twig->render('/public/modal/donate.modal.html.twig', $data);
-		}
-		return false;
+
+	    $data = [
+            'modal_id' => $this->id,
+            'header' => !empty($atts['header']) ? $atts['header'] : get_option('_kudos_form_header'),
+            'text' => !empty($atts['text']) ? $atts['text'] : get_option('_kudos_form_text'),
+            'color' => !empty($atts['color']) ? $atts['color'] : get_option('_kudos_button_color'),
+	        'nonce' => wp_nonce_field('kudos_submit', '_wpnonce', true, false),
+            'address' => [
+	            'enabled' => get_option('_kudos_address_enabled'),
+	            'required' => get_option('_kudos_address_required')
+			],
+		    'privacy_link' => $privacy_link,
+            'return_url' => $this->returnUrl,
+		    'donation_label' => $atts['donation_label'],
+	        'payment_by' => __('Secure payment by', 'kudos-donations'),
+	        'vendor' => (get_option('_kudos_payment_vendor') ? get_option('_kudos_payment_vendor') : 'mollie')
+	    ];
+
+		return $this->twig->render('/public/modal/donate.modal.html.twig', $data);
+
 	}
 }
