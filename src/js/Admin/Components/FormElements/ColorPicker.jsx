481fa3f9cc14735d9ea03b4d3407738893902757
michael <michael@iseardmedia.com>
2020-08-10 09:37:45 +0200

    Fix theme panel's colour select

--- src/js/Admin/Components/FormElements/ColorPicker.jsx
+++ src/js/Admin/Components/FormElements/ColorPicker.jsx
@@ -4,17 +4,12 @@ const { ColorPalette, BaseControl } = wp.components;
 
 const ColorPicker = ( props ) => {
 
-	const colors = [
-		{ name: 'orange', color: '#ff9f1c' },
-		{ name: 'green', color: '#2ec4b6' },
-	];
-
 	return (
 		<BaseControl
 			label={ __( 'Colour', 'kudos-donations' ) }
 		>
 			<ColorPalette
-				colors={ colors }
+				colors={ props.colors }
 				value={ props.value }
 				onChange={ ( value ) => props.onChange( props.id, value ) }
 				disableCustomColors={ props.disableCustomColors }
--- src/js/Admin/Components/Panels/ThemePanel.jsx
+++ src/js/Admin/Components/Panels/ThemePanel.jsx
@@ -5,12 +5,20 @@ const { PanelBody } = wp.components;
 import { ColorPicker } from '../FormElements/ColorPicker';
 
 const ThemePanel = (props ) => {
+
+	const colors = [
+		{ name: 'orange', color: '#ff9f1c' },
+		{ name: 'green', color: '#2ec4b6' },
+	];
+
 	return (
 		<PanelBody
 			title={ __( 'Theme Colour', 'kudos-donations' ) }
 			initialOpen={ false }
 		>
 			<ColorPicker
+				id="_kudos_theme_color"
+				colors={ colors }
 				value={ props.settings._kudos_theme_color }
 				onChange={ props.handleInputChange }
 				disableCustomColors
