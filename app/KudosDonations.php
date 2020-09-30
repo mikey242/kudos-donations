359922a4b10d8c21373bb70f0312253f861d9257
michael <michael@iseard.media>
2020-09-30 11:49:39 +0200

    Add check for action scheduler before including

--- app/KudosDonations.php
+++ app/KudosDonations.php
@@ -89,7 +89,7 @@ class KudosDonations {
 		$this->define_admin_hooks();
 		$this->define_public_hooks();
 
-		if ( Settings::get_setting( 'action_scheduler' ) ) {
+		if ( Settings::get_setting( 'action_scheduler' ) && file_exists(KUDOS_PLUGIN_DIR . '/libraries/action-scheduler/action-scheduler.php') ) {
 			require_once( KUDOS_PLUGIN_DIR . '/libraries/action-scheduler/action-scheduler.php' );
 		}
 
