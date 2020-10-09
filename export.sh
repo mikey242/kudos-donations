c8ab5a905037d6d01900fad5e6265604b688a801
michael <michael@iseard.media>
2020-10-09 14:50:40 +0200

    Add export script and update package.json commands

---
+++ export.sh
@@ -0,0 +1,20 @@
+echo "Exporting"
+# Remove existing export folder
+echo "Creating KUDOS_EXPORT folder"
+rm -rf ../KUDOS_EXPORT && \
+# Create new export folder
+mkdir -p ../KUDOS_EXPORT/kudos-donations/
+# Copy relevant files/folders to export
+echo "Copying assets to KUDOS_EXPORT"
+# shellcheck disable=SC2046
+cp -R $(<export-list.txt) ../KUDOS_EXPORT/kudos-donations/ && \
+# Remove unnecessary files
+echo "Cleaning up"
+cd ../KUDOS_EXPORT || exit
+rm -rf kudos-donations/libraries/action-scheduler/{docs,tests}
+rm kudos-donations/languages/*.json
+# Generating translations
+echo "Generating translations"
+wp i18n make-json kudos-donations/languages/
+# Creating kudos-donations.zip
+zip -qrD9 kudos-donations.zip kudos-donations
