d6bd54059dc167fb240ba633e172cd806828f0f9
michael <michael@iseard.media>
2020-10-09 13:25:49 +0200

    Fix incorrect languages path and add log message if not found

--- app/Service/I18nService.php
+++ app/Service/I18nService.php
@@ -2,25 +2,8 @@
 
 namespace Kudos\Service;
 
-/**
- * Define the internationalization functionality
- *
- * Loads and defines the internationalization files for this plugin
- * so that it is ready for translation.
- *
- * @link       https://www.linkedin.com/in/michael-iseard/
- * @since      1.0.0
- */
+class I18nService extends AbstractService {
 
-/**
- * Define the internationalization functionality.
- *
- * Loads and defines the internationalization files for this plugin
- * so that it is ready for translation.
- *
- * @since      1.0.0
- */
-class I18nService {
 
 	/**
 	 * Load the plugin text domain for translation.
@@ -29,13 +12,23 @@ class I18nService {
 	 */
 	public function load_plugin_textdomain() {
 
-		load_plugin_textdomain(
+		// Relative location of translations
+		$path = dirname( plugin_basename( __FILE__ ), 3 ) . '/languages';
+
+		// Load translations
+		$result = load_plugin_textdomain(
 			'kudos-donations',
 			false,
-			KUDOS_PLUGIN_DIR . '/languages/'
+			$path
 		);
 
+		// Log debug message if no local translation found
+		if(!$result) {
+			$this->logger->debug('Could not load plugin textdomain!', ['domain' => get_locale(),'path' => $path]);
+		}
+
 	}
+
 
 
 }
