7b0b9727621c1565e4b01ebf8df3e02505039ee0
michael <michael@iseard.media>
2020-10-12 18:32:08 +0200

    Actions now redirect to correct tab

--- app/Admin/partials/kudos-admin-debug.php
+++ app/Admin/partials/kudos-admin-debug.php
@@ -35,10 +35,11 @@ $tab         = isset( $_GET['tab'] ) ? $_GET['tab'] : $default_tab;
 
 		<?php
 
-        $url = admin_url( 'admin.php?page=kudos-debug&tab=actions' );
+		$url = admin_url( 'admin.php?page=kudos-debug' );
 
 		switch ( $tab ):
 			case 'actions':
+			    $url = add_query_arg('tab', 'actions', $url);
 				?>
                 <p>Please use the following actions only if you are having issues. Remember to backup your data before
                     performing any of these actions.</p>
@@ -91,6 +92,7 @@ $tab         = isset( $_GET['tab'] ) ? $_GET['tab'] : $default_tab;
 				break;
 
 			case 'log':
+				$url = add_query_arg('tab', 'log', $url);
 				$file = LoggerService::LOG_FILE;
 
 				// Quit if file does not exist
