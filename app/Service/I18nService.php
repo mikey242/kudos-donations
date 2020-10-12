ccfc4c15ef3a4a04072d7528f8f31cc10d0a78f2
michael <michael@iseard.media>
2020-10-12 18:33:15 +0200

    Remove unnecessary logging

--- app/Service/I18nService.php
+++ app/Service/I18nService.php
@@ -16,16 +16,11 @@ class I18nService extends AbstractService {
 		$path = dirname( plugin_basename( __FILE__ ), 3 ) . '/languages';
 
 		// Load translations
-		$result = load_plugin_textdomain(
+		load_plugin_textdomain(
 			'kudos-donations',
 			false,
 			$path
 		);
-
-		// Log debug message if no local translation found
-		if(!$result) {
-			$this->logger->debug('Could not load plugin textdomain!', ['domain' => get_locale(),'path' => $path]);
-		}
 
 	}
 
