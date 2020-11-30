2ce8ebe3db61fdb780a8b88d278e43afb594f85d
Michael Iseard <michael@iseardmedia.com>
2020-11-30 14:34:24 +0100

    Fix incorrect setting name

--- src/js/Settings/Components/Panels/CompletedPaymentPanel.jsx
+++ src/js/Settings/Components/Panels/CompletedPaymentPanel.jsx
@@ -33,7 +33,7 @@ const CompletedPaymentPanel = (props ) => {
 						type={ 'text' }
 						value={ props.settings._kudos_return_message_title || '' }
 						disabled={ props.isSaving }
-						onChange={ ( value ) => props.handleInputChange( "_kudos_return_message_header", value ) }
+						onChange={ ( value ) => props.handleInputChange( "_kudos_return_message_title", value ) }
 					/>
 
 					<TextControl
