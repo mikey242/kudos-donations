7f4da84ed992b8bd054af9b57998f4e6ad0c8706
Michael Iseard <michael@iseard.media>
2020-12-17 18:26:06 +0100

    Add lang.php file for gettext extraction

---
+++ templates/lang.php
@@ -0,0 +1,54 @@
+<?php
+// Template: "templates/emails/base.html.twig"
+__( 'All rights reserved.', 'kudos-donations' );
+
+// Template: "templates/emails/receipt.html.twig"
+__( 'This is a receipt for your recent donation on', 'kudos-donations' ); 
+__( 'Hi', 'kudos-donations' ); 
+__( 'Thanks for your donation. This email is a receipt for your records.', 'kudos-donations' ); 
+__( 'Order ID', 'kudos-donations' ); 
+__( 'Date', 'kudos-donations' ); 
+__( 'Description', 'kudos-donations' ); 
+__( 'Amount', 'kudos-donations' ); 
+__( 'Total', 'kudos-donations' ); 
+__( 'Thanks', 'kudos-donations' ); 
+__( 'Want to cancel your subscription?', 'kudos-donations' ); 
+__( 'Click here', 'kudos-donations' );
+
+// Template: "templates/public/modal/donate.modal.html.twig"
+__('Other', 'kudos-donations');
+__('Amount', 'kudos-donations');
+__('E-mail address', 'kudos-donations');
+__('One-off', 'kudos-donations');
+__('Recurring', 'kudos-donations');
+__('Subscription', 'kudos-donations');
+__('How often would you like to donate?', 'kudos-donations');
+__('Payment frequency', 'kudos-donations');
+__('Yearly', 'kudos-donations');
+__('Quarterly', 'kudos-donations');
+__('Monthly', 'kudos-donations');
+__('Continuous', 'kudos-donations');
+__('year', 'kudos-donations');
+__('years', 'kudos-donations');
+__('Address', 'kudos-donations');
+__('Please fill in you address', 'kudos-donations');
+__('Street', 'kudos-donations');
+__('Postcode', 'kudos-donations');
+__('City', 'kudos-donations');
+__('Country', 'kudos-donations');
+__('Payment', 'kudos-donations');
+__('By clicking donate you agree to the following payment', 'kudos-donations');
+__('Name', 'kudos-donations');
+__('Email', 'kudos-donations');
+__('Type', 'kudos-donations');
+__( 'I agree with the %s ', "kudos-donations" );
+__( 'I agree with the privacy policy.', "kudos-donations" );
+__('Donate', 'kudos-donations');
+
+// Template: "templates/public/modal/message.modal.html.twig"
+__( 'Ok', 'kudos-donations' );
+
+// Template: "templates/public/modal/embeds/_tab.html.twig"
+__( 'Cancel', 'kudos-donations' ); 
+__( 'Previous tab', 'kudos-donations' ); 
+__( 'Next', 'kudos-donations' );
