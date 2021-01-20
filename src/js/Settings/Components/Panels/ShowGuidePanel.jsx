c538eec3f6063ed4a9a5a5d73e594ead672bfb41
Michael Iseard <michael@iseard.media>
2021-01-20 12:10:13 +0100

    Add ShowGuide panel to advanced menu

---
+++ src/js/Settings/Components/Panels/ShowGuidePanel.jsx
@@ -0,0 +1,28 @@
+const { __ } = wp.i18n;
+const { PanelBody, Button, BaseControl } = wp.components;
+
+const ShowGuidePanel = ( { handleInputChange } ) => {
+
+    return (
+        <PanelBody
+            title={ __( 'Show guide', 'kudos-donations' ) }
+            initialOpen={ false }
+        >
+            <BaseControl
+                id="export-1"
+                help={__( 'Opens the guide that was displayed when Kudos Donations was first installed.', 'kudos-donations' )}
+            >
+                <Button
+                    isLink
+                    onClick={ () => {
+                        handleInputChange('_kudos_show_intro', true);
+                    } }
+                >
+                    { __( 'Show guide', 'kudos-donations' ) }
+                </Button>
+            </BaseControl>
+        </PanelBody>
+    );
+};
+
+export { ShowGuidePanel };
