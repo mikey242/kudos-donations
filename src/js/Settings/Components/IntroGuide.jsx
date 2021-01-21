e6ba0d990970cb2f2209bbd63d0f6f380333adf7
Michael Iseard <michael@iseard.media>
2021-01-21 13:38:54 +0100

    Make all text translatable and use ExternalLink component for links

--- src/js/Settings/Components/IntroGuide.jsx
+++ src/js/Settings/Components/IntroGuide.jsx
@@ -5,7 +5,7 @@ import button from "../../../img/guide-button.png"
 import live from "../../../img/guide-test-live.png"
 
 const { __ } = wp.i18n;
-const { Guide } = wp.components;
+const { Guide, ExternalLink } = wp.components;
 
 const IntroGuide = ({ show, updateSetting } ) => {
 
@@ -23,52 +23,54 @@ const IntroGuide = ({ show, updateSetting } ) => {
             onFinish={ () => closeModal() }
             pages={ [
                 {
-                    image: <img src={intro} />,
+                    image: <img alt="Intro graphic" src={intro} />,
                     content:
                     <div className="kd-intro-container">
                         <h1 className="kd-leading-normal">{__('Welcome to Kudos Donations', 'kudos-donations')}</h1>
-                        <p>It is easy to get started with Kudos Donations.</p>
-                        <p>Complete these simple steps to get your donations started.</p>
+                        <p>{__('It is easy to get started with Kudos Donations.', 'kudos-donations')}</p>
+                        <p>{__('Complete these simple steps to get your donations started.', 'kudos-donations')}</p>
                     </div>
                     ,
                 },
                 {
-                    image: <img src={mollie} />,
+                    image: <img alt="Mollie API key graphic" src={mollie} />,
                     content:
                         <div className="kd-intro-container">
-                            <h1 className="kd-leading-normal">Connect with Mollie</h1>
-                            <p>Log-in to your Mollie account and grab your <a target="_blank" href="https://mollie.com/dashboard/developers/api-keys">api keys</a>.</p>
-                            <p>Make sure you get both your <strong>test</strong> and <strong>live</strong> api keys. Then enter them in the relevant fields.</p>
+                            <h1 className="kd-leading-normal">{__('Connect with Mollie', 'kudos-donations')}</h1>
+                            <p>{__('Login to your Mollie account and grab your api keys.', 'kudos-donations')}</p>
+                            <p>{__('Make sure you get both your test and live api keys. Then enter them under the Mollie tab.', 'kudos-donations')}</p>
+                            <ExternalLink href="https://mollie.com/dashboard/developers/api-keys">{__('Mollie dashboard', 'kudos-donations')}</ExternalLink>
                         </div>
                     ,
                 },
                 {
-                    image: <img src={campaign} />,
+                    image: <img alt="Campaign graphic" src={campaign} />,
                     content:
                         <div className="kd-intro-container">
-                            <h1 className="kd-leading-normal">Set up a campaign</h1>
-                            <p>Visit the <strong>Campaigns</strong> tab and either create a new campaign or edit the default one. </p>
-                            <p>If you need it, don't forget to click <strong>Copy shortcode</strong> at the bottom of your campaign.</p>
+                            <h1 className="kd-leading-normal">{__('Set up a campaign', 'kudos-donations')}</h1>
+                            <p>{__('Visit the Campaigns tab and either create a new campaign or edit the default one.', 'kudos-donations')}</p>
+                            <p>{__('If you need it, don\'t forget to click "Copy shortcode" at the bottom of your campaign.', 'kudos-donations')}</p>
                         </div>
                     ,
                 },
                 {
-                    image: <img src={button} />,
+                    image: <img alt="Kudos Donations button graphic" src={button} />,
                     content:
                         <div className="kd-intro-container">
-                            <h1 className="kd-leading-normal">Place a button</h1>
-                            <p>Using the Kudos Button block or shortcode, place the button anywhere on your website.</p>
-                            <p>If using the block, you will need to select the campaign you created.</p>
+                            <h1 className="kd-leading-normal">{__('Place a button', 'kudos-donations')}</h1>
+                            <p>{__('Using the Kudos Button block or shortcode, place the button anywhere on your website.', 'kudos-donations')}</p>
+                            <p>{__('If using the block, you will need to select the campaign you created.', 'kudos-donations')}</p>
                         </div>
                     ,
                 },
                 {
-                    image: <img src={live} />,
+                    image: <img alt="Switch for live mode" src={live} />,
                     content:
                         <div className="kd-intro-container">
-                            <h1 className="kd-leading-normal">Test and go Live</h1>
-                            <p>With the API mode still on <strong>Test</strong> make a payment using your button. If it all looks good then you can switch to <strong>Live</strong>.</p>
-                            <p>For more information visit our <a target="_blank" href="https://kudosdonations.com/faq/">F.A.Q</a>.</p>
+                            <h1 className="kd-leading-normal">{__('Test and go Live', 'kudos-donations')}</h1>
+                            <p>{__('With the API mode still on "Test" make a payment using your button. If it all looks good then you can switch to "Live".', 'kudos-donations')}</p>
+                            <p>{__('Good luck with your campaign!', 'kudos-donations')}</p>
+                            <p><ExternalLink href="https://kudosdonations.com/faq/">Visit our F.A.Q</ExternalLink></p>
                         </div>
                     ,
                 },
