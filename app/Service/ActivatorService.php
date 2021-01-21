93cca01d331ebfe3ab67d00c0a508ea9dc29ac21
Michael Iseard <michael@iseard.media>
2021-01-21 17:57:28 +0100

    Change campaign to campaign_id

--- app/Service/ActivatorService.php
+++ app/Service/ActivatorService.php
@@ -133,7 +133,7 @@ class ActivatorService {
 		  transaction_id VARCHAR(255),
 		  subscription_id VARCHAR(255),
 		  refunds BLOB DEFAULT NULL,
-		  campaign VARCHAR(255),
+		  campaign_id VARCHAR(255),
 		  secret VARCHAR(255),
 		  PRIMARY KEY (id)
 		) $charset_collate;";
