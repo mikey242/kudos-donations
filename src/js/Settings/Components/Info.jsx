4783ca69d707cd28df91e288bb9ce7e5f3553827
Michael Iseard <michael@iseard.media>
2021-01-26 10:47:09 +0100

    Remove PanelRow component

--- src/js/Settings/Components/Info.jsx
+++ src/js/Settings/Components/Info.jsx
@@ -1,18 +1,12 @@
-const {PanelRow, Dashicon} = wp.components
+const {Dashicon} = wp.components
 
 const Info = (props) => {
 
     return (
-
-        <PanelRow>
-
-            <div className="kudos-admin-info kd-text-gray-500 kd-flex kd-items-center kd-justify-center">
-                <Dashicon icon={props.icon ?? "info"}/>
-                <i>{props.children}</i>
-            </div>
-
-        </PanelRow>
-
+        <div className="kudos-admin-info kd-text-gray-500 kd-flex kd-items-center kd-justify-start">
+            <Dashicon icon={props.icon ?? "info"}/>
+            <i>{props.children}</i>
+        </div>
     )
 
 }
