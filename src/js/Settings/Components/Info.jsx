235c0df4c93a82d83005085d8e82be8bed7b62f9
Michael Iseard <michael@iseard.media>
2021-01-26 11:03:16 +0100

    Add extra margin between icon and text

--- src/js/Settings/Components/Info.jsx
+++ src/js/Settings/Components/Info.jsx
@@ -4,7 +4,7 @@ const Info = (props) => {
 
     return (
         <div className="kudos-admin-info kd-text-gray-500 kd-flex kd-items-center kd-justify-start">
-            <Dashicon icon={props.icon ?? "info"}/>
+            <Dashicon className="kd-mr-1" icon={props.icon ?? "info"}/>
             <i>{props.children}</i>
         </div>
     )
