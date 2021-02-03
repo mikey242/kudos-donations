58adac50c0ebec80781f78753860f3085d0f1e22
Michael Iseard <michael@iseard.media>
2021-02-03 13:54:54 +0100

    Add filter to settingCard

--- src/js/Settings/Components/SettingCard.jsx
+++ src/js/Settings/Components/SettingCard.jsx
@@ -1,11 +1,14 @@
 const {CardBody} = wp.components
+const {applyFilters} = wp.hooks
 
-const SettingCard = ({ title, children }) => {
+const SettingCard = ({ title, children, id }) => {
+
+    let filtered = applyFilters('kudos.settings.settingCard.' + id, {title: title, children: children})
 
     return (
         <CardBody size="medium">
-            <h3 className="kd-mb-5">{title}</h3>
-            { children }
+            <h3 className="kd-mb-5">{filtered.title}</h3>
+            {filtered.children}
         </CardBody>
     )
 }
