5735884566b9a60d2bbe07984bc6b4d60420631e
Michael Iseard <michael@iseard.media>
2021-02-08 11:05:54 +0100

    Merge help and advanced tabs

--- src/js/Settings/Components/Tabs/HelpTab.jsx
+++ src/js/Settings/Components/Tabs/HelpTab.jsx
@@ -1,16 +1,38 @@
 import {HelpPanel} from "../Panels/HelpPanel"
+import {ExportSettingsPanel} from "../Panels/ExportSettingsPanel"
+import {ImportSettingsPanel} from "../Panels/ImportSettingsPanel"
+import {DebugModePanel} from "../Panels/DebugModePanel"
 
 
-const {Card} = wp.components
+const {Card, CardDivider} = wp.components
+const {Fragment} = wp.element
 
 const HelpTab = (props) => {
 
     return (
-        <Card>
-            <HelpPanel
-                updateSetting={props.updateSetting}
-            />
-        </Card>
+        <Fragment>
+            <Card>
+                <HelpPanel
+                    updateSetting={props.updateSetting}
+                />
+            </Card>
+            <br/>
+            <Card>
+                <ExportSettingsPanel
+                    settings={props.settings}
+                />
+                <CardDivider/>
+                <ImportSettingsPanel
+                    updateAll={props.updateAll}
+                    handleInputChange={props.handleInputChange}
+                />
+                <CardDivider/>
+                <DebugModePanel
+                    settings={props.settings}
+                    updateSetting={props.updateSetting}
+                />
+            </Card>
+        </Fragment>
     )
 }
 
