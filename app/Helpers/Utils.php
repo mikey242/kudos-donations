546d41b323bb78c7a2357464b14a971482286f23
Michael Iseard <michael@iseard.media>
2021-03-04 13:19:22 +0100

    Add get_asset_content function for future use

--- app/Helpers/Utils.php
+++ app/Helpers/Utils.php
@@ -58,6 +58,34 @@ class Utils {
 	}
 
 	/**
+	 * Get plugin asset content via a hashed filename.
+	 *
+	 * Checks for a hashed filename as a value in a JSON object.
+	 * If it exists: the hashed filename is enqueued in place of the asset name.
+	 * Fallback: the default asset name will be passed through.
+	 *
+	 * @source https://danielshaw.co.nz/wordpress-cache-busting-json-hash-map/
+	 * @param string $asset e.g style.css.
+	 *
+	 * @return string
+	 * @since   1.0.0
+	 */
+	public static function get_asset_content( string $asset ): string {
+
+		$map      = KUDOS_PLUGIN_URL . 'dist/assets-manifest.json';
+		$request  = wp_remote_get( $map );
+		$response = wp_remote_retrieve_body( $request );
+		$hash     = ! empty( $response ) ? json_decode( $response, true ) : [];
+
+		if ( array_key_exists( $asset, $hash ) ) {
+				$asset_request = wp_remote_get(KUDOS_PLUGIN_URL . 'dist/' . $hash[ $asset ]);
+				return wp_remote_retrieve_body( $asset_request );
+		}
+
+		return $asset;
+	}
+
+	/**
 	 * Converts three letter currency code into a symbol
 	 *
 	 * @param string $currency Three letter currency code (EUR, GBP, USD).
