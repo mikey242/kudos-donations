309197ca2cd45e5f45d411a4a40385861d55f8a0
Michael Iseard <michael@iseard.media>
2021-03-23 10:50:04 +0100

    Remove unused postcss plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,5 @@
 module.exports = {
     plugins: [
-        require('postcss-nesting'),
         require('tailwindcss'),
         require('postcss-add-root-selector')({
             rootSelector: '.kudos-donations',
