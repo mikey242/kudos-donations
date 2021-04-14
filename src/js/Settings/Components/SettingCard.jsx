019fa73bdfe79f2dbbe4835666c03e26f641ac71
Michael Iseard <michael@iseard.media>
2021-04-14 15:40:40 +0200

    Refactor SettingCard. Allowing for description prop and easier filter usage.

--- src/js/Settings/Components/SettingCard.jsx
+++ src/js/Settings/Components/SettingCard.jsx
@@ -1,15 +1,22 @@
 const {CardBody} = wp.components
+const {Fragment} = wp.element
 const {applyFilters} = wp.hooks
 
 const SettingCard = (props) => {
 
-    let filtered = applyFilters('kudos.settings.settingCard.' + props.id, {...props})
 
     return (
-        <CardBody size="medium">
-            <h3 className="kd-mb-5">{filtered.title}</h3>
-            {filtered.children}
-        </CardBody>
+        applyFilters('kudos.settings.settingCard.' + props.id,
+            <Fragment>
+                <CardBody size="medium">
+                    <header className={"kd-mb-6"}>
+                        <h3 className="kd-mb-5">{props.title}</h3>
+                        {/*<p className={"kd-mb-6 kd-text-lg kd-text-gray-500"}>{props.description}</p>*/}
+                    </header>
+                    {props.children}
+                </CardBody>
+            </Fragment>,
+            props)
     )
 }
 
