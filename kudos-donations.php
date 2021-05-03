df7cbfe9d60bfa394eb486794d17f4f41b0065b9
Michael Iseard <michael@iseard.media>
2021-05-03 12:01:56 +0200

    Update min php version to 7.2

--- kudos-donations.php
+++ kudos-donations.php
@@ -14,7 +14,7 @@
  * Author:            Iseard Media
  * Author URI:        https://iseard.media
  * Requires at least: 5.5
- * Requires PHP:      7.1
+ * Requires PHP:      7.2
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       kudos-donations
