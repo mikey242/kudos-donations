55504c61062f55cc625d0c8eb6566669e2043ce5
michael <michael@iseard.media>
2021-08-02 11:19:46 +0200

    Use logo component instead of <img> to prevent border on WordPress 5.8

--- src/blocks/kudos-button/index.jsx
+++ src/blocks/kudos-button/index.jsx
@@ -1,13 +1,13 @@
 import {registerBlockType} from '@wordpress/blocks'
 import Save from "./save"
 import Edit from "./edit"
-import logo from '../../images/logo-colour.svg'
+import {KudosLogo} from "../../common/components/KudosLogo";
 
 /**
  * Register block.
  */
 export default registerBlockType('iseardmedia/kudos-button', {
-    icon: <img width="30" src={logo} alt="Kudos Logo"/>,
+    icon: <KudosLogo/>,
     edit: Edit,
     save: Save
 })
