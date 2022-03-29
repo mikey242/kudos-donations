8a316efea3a0017d83e2cd73fe3ba8f8ab23efd5
Michael Iseard <michael@iseard.media>
2022-03-29 13:34:32 +0200

    Fix phpmailer not correctly setting isHTML

--- app/Service/MailerService.php
+++ app/Service/MailerService.php
@@ -68,9 +68,11 @@ class MailerService {
 			'kudos-logo.png'
 		);
 
+		// Enable HTML email support to header.
+		$phpmailer->isHTML( true );
+
 		// Add custom config if enabled.
 		if ( Settings::get_setting( 'smtp_enable' ) ) {
-			$phpmailer->isSMTP();
 			$phpmailer->isHTML( true );
 			$phpmailer->Host        = Settings::get_setting( 'smtp_host' );
 			$phpmailer->SMTPAutoTLS = Settings::get_setting( 'smtp_autotls' );
