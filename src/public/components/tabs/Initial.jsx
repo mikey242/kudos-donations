bba382136146aaa26d1ea7350377139b3f2d496b
Michael Iseard <michael@iseard.media>
2022-05-19 12:04:50 +0200

    ESLint

--- src/public/components/tabs/Initial.jsx
+++ src/public/components/tabs/Initial.jsx
@@ -1,82 +1,124 @@
-import { __ } from '@wordpress/i18n'
-import React from 'react'
-import { useFormContext, useWatch } from 'react-hook-form'
-import FormTab from './FormTab'
-import { useEffect } from '@wordpress/element'
-import { RadioGroupControl, TextControl, ToggleControl } from '../../../common/components/controls'
-import { ProgressBar } from '../ProgressBar'
+import { __ } from '@wordpress/i18n';
+import React from 'react';
+import { useFormContext, useWatch } from 'react-hook-form';
+import FormTab from './FormTab';
+import { useEffect } from '@wordpress/element';
+import {
+	RadioGroupControl,
+	TextControl,
+	ToggleControl,
+} from '../../../common/components/controls';
+import { ProgressBar } from '../ProgressBar';
 
 const Initial = (props) => {
-  const { title, description, buttons, donationType, amountType, fixedAmounts, goal, showGoal, total } = props
+	const {
+		title,
+		description,
+		buttons,
+		donationType,
+		amountType,
+		fixedAmounts,
+		goal,
+		showGoal,
+		total,
+	} = props;
 
-  const {
-    setValue
-  } = useFormContext()
+	const { setValue } = useFormContext();
 
-  const watchFixed = useWatch({ name: 'valueFixed' })
-  const watchValue = useWatch({ name: 'value' })
+	const watchFixed = useWatch({ name: 'valueFixed' });
+	const watchValue = useWatch({ name: 'value' });
 
-  useEffect(() => {
-    if (donationType !== 'both') {
-      setValue('recurring', donationType === 'recurring')
-    }
-  }, [])
+	useEffect(() => {
+		if (donationType !== 'both') {
+			setValue('recurring', donationType === 'recurring');
+		}
+	}, []);
 
-  useEffect(() => {
-    if (watchFixed) {
-      setValue('value', watchFixed)
-    }
-  }, [watchFixed])
+	useEffect(() => {
+		if (watchFixed) {
+			setValue('value', watchFixed);
+		}
+	}, [watchFixed]);
 
-  useEffect(() => {
-    setValue('valueFixed', watchValue)
-  }, [watchValue])
+	useEffect(() => {
+		setValue('valueFixed', watchValue);
+	}, [watchValue]);
 
-  return (
-        <FormTab
-            title={title}
-            description={description}
-            buttons={buttons}
-        >
-            {showGoal && <ProgressBar goal={goal} total={total} extra={watchValue}/>}
-            {(amountType === 'both' || amountType === 'fixed') &&
-                <RadioGroupControl name="valueFixed" options={
-                    fixedAmounts.map((value) => {
-                      return { value: value, label: '€' + value }
-                    })
-                }/>
-            }
+	return (
+		<FormTab title={title} description={description} buttons={buttons}>
+			{showGoal && (
+				<ProgressBar goal={goal} total={total} extra={watchValue} />
+			)}
+			{(amountType === 'both' || amountType === 'fixed') && (
+				<RadioGroupControl
+					name="valueFixed"
+					options={fixedAmounts.map((value) => {
+						return { value, label: '€' + value };
+					})}
+				/>
+			)}
 
-            {(amountType === 'both' || amountType === 'open') &&
-                <TextControl name="value"
-                             addOn="€"
-                             validation={{
-                               required: __('Minimum donation is 1 euro', 'kudos-donations'),
-                               min: { value: 1, message: __('Minimum donation is 1 euro', 'kudos-donations') },
-                               max: { value: 5000, message: __('Maximum donation is 5000 euros', 'kudos-donations') }
-                             }}
-                             type="number" placeholder={
-                    `${amountType === 'both' ? __('Other amount', 'kudos-donations') : __('Amount', 'kudos-donations')}`
-                }/>
-            }
+			{(amountType === 'both' || amountType === 'open') && (
+				<TextControl
+					name="value"
+					addOn="€"
+					validation={{
+						required: __(
+							'Minimum donation is 1 euro',
+							'kudos-donations'
+						),
+						min: {
+							value: 1,
+							message: __(
+								'Minimum donation is 1 euro',
+								'kudos-donations'
+							),
+						},
+						max: {
+							value: 5000,
+							message: __(
+								'Maximum donation is 5000 euros',
+								'kudos-donations'
+							),
+						},
+					}}
+					type="number"
+					placeholder={`${
+						amountType === 'both'
+							? __('Other amount', 'kudos-donations')
+							: __('Amount', 'kudos-donations')
+					}`}
+				/>
+			)}
 
-            <TextControl name="name"
-                         validation={{ required: __('Your name is required', 'kudos-donations') }}
-                         placeholder={__('Name', 'kudos-donations')}/>
+			<TextControl
+				name="name"
+				validation={{
+					required: __('Your name is required', 'kudos-donations'),
+				}}
+				placeholder={__('Name', 'kudos-donations')}
+			/>
 
-            <TextControl name="email_address"
-                         validation={{ required: __('Your email is required', 'kudos-donations') }}
-                         type="email" placeholder={__('Email', 'kudos-donations')}/>
+			<TextControl
+				name="email_address"
+				validation={{
+					required: __('Your email is required', 'kudos-donations'),
+				}}
+				type="email"
+				placeholder={__('Email', 'kudos-donations')}
+			/>
 
-            {donationType === 'both' &&
-                <div className="flex justify-center mt-3">
-                    <ToggleControl name="recurring" validation={{ required: true }}
-                                   label={__('Recurring donation', 'kudos-donations')}/>
-                </div>
-            }
+			{donationType === 'both' && (
+				<div className="flex justify-center mt-3">
+					<ToggleControl
+						name="recurring"
+						validation={{ required: true }}
+						label={__('Recurring donation', 'kudos-donations')}
+					/>
+				</div>
+			)}
+		</FormTab>
+	);
+};
 
-        </FormTab>
-  )
-}
-
-export default Initial
+export default Initial;
