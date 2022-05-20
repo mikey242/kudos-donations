25a2c36dea2bec2c5d498df7852bc1a3b137a835
Michael Iseard <michael@iseard.media>
2022-05-20 10:18:41 +0200

    Remove unused <defs/>

--- app/Controller/Admin.php
+++ app/Controller/Admin.php
@@ -92,7 +92,7 @@ class Admin
             $parent_slug,
             false,
             'data:image/svg+xml;base64,' . base64_encode(
-                '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 555 449"><defs/><path fill="#f0f5fa99" d="M0-.003h130.458v448.355H.001zM489.887 224.178c78.407 47.195 78.407 141.59 39.201 188.784-39.2 47.194-117.612 47.194-196.019 0-58.809-33.04-117.612-117.992-156.818-188.784 39.206-70.793 98.01-155.744 156.818-188.781 78.407-47.196 156.818-47.196 196.02 0 39.205 47.195 39.205 141.587-39.202 188.781z"/></svg>'
+                '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 555 449"><path fill="#f0f5fa99" d="M0-.003h130.458v448.355H.001zM489.887 224.178c78.407 47.195 78.407 141.59 39.201 188.784-39.2 47.194-117.612 47.194-196.019 0-58.809-33.04-117.612-117.992-156.818-188.784 39.206-70.793 98.01-155.744 156.818-188.781 78.407-47.196 156.818-47.196 196.02 0 39.205 47.195 39.205 141.587-39.202 188.781z"/></svg>'
             )
         );
 
@@ -563,7 +563,7 @@ class Admin
                     }
                     new AdminNotice(__('No transactions need adding', 'kudos-donations'));
                     break;
-                case 'kudos_migrate_campaigns';
+                case 'kudos_migrate_campaigns':
                     $old_campaigns = Settings::get_setting('campaigns');
                     if ($old_campaigns) {
                         $success = 0;
