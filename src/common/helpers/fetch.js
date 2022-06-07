92d0e23be3077e803624b94b39502e95ef207375
Michael Iseard <michael@iseard.media>
2022-06-07 10:09:13 +0200

    Remove unused export

--- src/common/helpers/fetch.js
+++ src/common/helpers/fetch.js
@@ -1,12 +1,5 @@
 import apiFetch from '@wordpress/api-fetch';
 
-export async function fetchCampaignTransactions(id) {
-	return await apiFetch({
-		path: `kudos/v1/transaction/campaign/${id ?? ''}`,
-		method: 'GET',
-	});
-}
-
 export async function fetchCampaigns(id) {
 	return await apiFetch({
 		path: `wp/v2/kudos_campaign/${id ?? ''}`,
