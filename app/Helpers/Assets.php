feb7c6d2a6a0ef03f966512fd91408c2e6db3cd6
Michael Iseard <michael@iseard.media>
2022-06-13 09:32:31 +0200

    Remove unused functions

--- app/Helpers/Assets.php
+++ app/Helpers/Assets.php
@@ -5,27 +5,6 @@ namespace Kudos\Helpers;
 class Assets
 {
     /**
-     * Serve plugin assets via a hashed filename.
-     *
-     * Checks for a hashed filename as a value in a JSON object.
-     * If it exists: the hashed filename is enqueued in place of the asset name.
-     * Fallback: the default asset name will be passed through.
-     *
-     * @source https://danielshaw.co.nz/wordpress-cache-busting-json-hash-map/
-     * @param string $url e.g. style.css.
-     *
-     * @return array
-     */
-    private static function get_asset_manifest(string $url): array
-    {
-        $map      = $url . 'dist/mix-manifest.json';
-        $request  = wp_remote_get($map);
-        $response = wp_remote_retrieve_body($request);
-
-        return ! empty($response) ? json_decode($response, true) : [];
-    }
-
-    /**
      * Uses manifest to get asset URL.
      *
      * @param string $asset
@@ -35,11 +14,6 @@ class Assets
      */
     public static function get_asset_url(string $asset, string $url = KUDOS_PLUGIN_URL): string
     {
-        $hash = self::get_asset_manifest($url);
-        if (isset($hash[$asset])) {
-            return $url . 'dist/' . ltrim($hash[$asset], '/');
-        }
-
         return $url . 'dist/' . ltrim($asset, '/');
     }
 
@@ -80,40 +54,11 @@ class Assets
      * Uses manifest to get asset path.
      *
      * @param string $asset
-     * @param string $url
      *
      * @return string
      */
-    public static function get_asset_path(string $asset, string $url = KUDOS_PLUGIN_URL): string
+    public static function get_asset_path(string $asset): string
     {
-        $hash = self::get_asset_manifest($url);
-        if (isset($hash[$asset])) {
-            return KUDOS_PLUGIN_DIR . '/dist/' . ltrim($hash[$asset], '/');
-        }
-
         return KUDOS_PLUGIN_DIR . '/dist/' . ltrim($asset, '/');
-    }
-
-    /**
-     * Get plugin asset content via a hashed filename.
-     *
-     * Checks for a hashed filename as a value in a JSON object.
-     * If it exists: the hashed filename is enqueued in place of the asset name.
-     * Fallback: the default asset name will be passed through.
-     *
-     * @source https://danielshaw.co.nz/wordpress-cache-busting-json-hash-map/
-     * @param string $asset e.g. style.css.
-     * @param string $base
-     *
-     * @return string
-     */
-    public static function get_asset_content(string $asset, string $base = KUDOS_PLUGIN_URL): string
-    {
-        $hash = self::get_asset_manifest($base);
-        $file = $hash[$asset] ?? $asset;
-
-        $asset_request = wp_remote_get(KUDOS_PLUGIN_URL . 'dist/' . $file);
-
-        return wp_remote_retrieve_body($asset_request);
     }
 }
