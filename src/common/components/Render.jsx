6b25d051bf0c427b6d3fd7a6f6ae0129258741a4
Michael Iseard <michael@iseard.media>
2022-06-24 17:40:45 +0200

    Render component now waits for stylsheet to prevent FOUC

--- src/common/components/Render.jsx
+++ src/common/components/Render.jsx
@@ -1,21 +1,36 @@
 import React from 'react';
 import ReactShadowRoot from 'react-shadow-root';
 import classNames from 'classnames';
+import { useState } from '@wordpress/element';
 
 function Render({ children, themeColor, stylesheet, style, className }) {
+	const [ready, setReady] = useState(false);
 	return (
-		<ReactShadowRoot>
-			<link rel="stylesheet" href={stylesheet} />
-			{style && <style>{style}</style>}
-			{themeColor && (
-				<style>{`:host {--kudos-theme-primary: ${themeColor}`}</style>
-			)}
-			<div id="kudos-container">
-				<div id="kudos" className={classNames(className, 'font-sans')}>
-					{children}
-				</div>
-			</div>
-		</ReactShadowRoot>
+		<>
+			<ReactShadowRoot>
+				{stylesheet && (
+					<link
+						rel="stylesheet"
+						onLoad={() => setReady(true)}
+						href={stylesheet}
+					/>
+				)}
+				{style && <style>{style}</style>}
+				{themeColor && (
+					<style>{`:host {--kudos-theme-primary: ${themeColor}`}</style>
+				)}
+				{ready && (
+					<div id="kudos-container">
+						<div
+							id="kudos"
+							className={classNames(className, 'font-sans')}
+						>
+							{children}
+						</div>
+					</div>
+				)}
+			</ReactShadowRoot>
+		</>
 	);
 }
 
