aba3e69d6ae7e41332a4e682e955e6f7daf19e33
Michael Iseard <michael@iseard.media>
2022-06-29 12:42:12 +0200

    Add isExternal prop to external links

--- src/admin/components/settings/tabs/HelpTab.jsx
+++ src/admin/components/settings/tabs/HelpTab.jsx
@@ -56,7 +56,7 @@ const HelpTab = ({ setShowIntro }) => {
 						isOutline
 						className="mr-2"
 						href="https://wordpress.org/support/plugin/kudos-donations/"
-						target="_blank"
+						isExternal
 					>
 						<SupportIcon className="hidden sm:block w-5 h-5 mr-2" />
 						{__('Support forums', 'kudos-donations')}
@@ -72,7 +72,7 @@ const HelpTab = ({ setShowIntro }) => {
 					<Button
 						isOutline
 						className={'mr-2'}
-						target="_blank"
+						isExternal
 						href="https://kudosdonations.com/faq/"
 					>
 						<QuestionMarkCircleIcon className="hidden sm:block w-5 h-5 mr-2" />
