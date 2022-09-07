42b162fafab660a3a74ced853f7ef2b69726499e
Michael Iseard <michael@iseard.media>
2022-09-07 12:59:06 +0200

    Add Posts helper for potential future use

---
+++ app/Helpers/Posts.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Kudos\Helpers;
+
+class Posts
+{
+
+    public static function get_shortcodes_on_page($post_id, $shortcode)
+    {
+        // Get the post content once
+        $content = get_the_content(null, false, $post_id);
+
+        // Double check that there is content
+        if ($content) {
+            // Shortcode regex
+            $shortcode_regex = '/\[' . $shortcode . '\s.*?]/';
+
+            // Get all the shortcodes from the page
+            if (preg_match_all($shortcode_regex, $content, $shortcodes)) {
+                // Store them here
+                $final_array = [];
+
+                // Extract the attributes from the shortcode
+                foreach ($shortcodes[0] as $s) {
+                    $attributes = self::shortcode_parse_atts($s);
+
+                    // The return the post
+                    $final_array[] = $attributes;
+                }
+
+                // Return the array
+                $results = $final_array;
+                // Otherwise return an empty array if none are found
+            } else {
+                $results = [];
+            }
+
+            // Return it
+            return $results;
+        } else {
+            return false;
+        }
+    }
+
+    public static function shortcode_parse_atts($shortcode): array
+    {
+        // Store the shortcode attributes in an array here
+        $attributes = [];
+
+        if (preg_match_all('/\w+\=\".*?\"/', $shortcode, $key_value_pairs)) {
+            // Now split up the key value pairs
+            foreach ($key_value_pairs[0] as $kvp) {
+                $kvp                  = str_replace('"', '', $kvp);
+                $pair                 = explode('=', $kvp);
+                $attributes[$pair[0]] = $pair[1];
+            }
+        }
+
+        // Return the array
+        return $attributes;
+    }
+}
