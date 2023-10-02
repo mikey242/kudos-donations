30751810bd535f165874fdfa629bf509a43d1fe9
michael <michael@iseard.media>
2023-10-02 11:31:24 +0200

    fix: remove unused helper functions

--- src/helpers/util.js
+++ src/helpers/util.js
@@ -1,29 +1,3 @@
-export function camelize(str) {
-	return str
-		.replace(/(^\w|[A-Z]|\b\w)/g, function (word, index) {
-			return index === 0 ? word.toLowerCase() : word.toUpperCase();
-		})
-		.replace(/\s+/g, '');
-}
-
-export function getQueryVar(key, fallback = null) {
-	const searchParams = new URLSearchParams(window.location.search);
-	if (searchParams.has(key)) {
-		return searchParams.get(key);
-	}
-	return fallback;
-}
-
-export function updateQueryParameter(key, value) {
-	if ('URLSearchParams' in window) {
-		const searchParams = new URLSearchParams(window.location.search);
-		searchParams.set(key, value?.toString());
-		const newRelativePathQuery =
-			window.location.pathname + '?' + searchParams.toString();
-		history.replaceState(null, '', newRelativePathQuery);
-	}
-}
-
 export function removeQueryParameters(keys) {
 	keys.forEach((key) => {
 		if ('URLSearchParams' in window) {
@@ -34,32 +8,6 @@ export function removeQueryParameters(keys) {
 			history.replaceState(null, '', newRelativePathQuery);
 		}
 	});
-}
-
-export function getStyle(style) {
-	return getComputedStyle(document.documentElement).getPropertyValue(style);
-}
-
-export function setAttributes(el, attrs) {
-	Object.entries(attrs).forEach(([key, value]) =>
-		el.setAttribute(key, value)
-	);
-}
-
-export function isVisible(el) {
-	// Check if element visible
-	if (el.offsetParent === null) return false;
-
-	// Check if in viewport
-	const rect = el.getBoundingClientRect();
-	return (
-		rect.top >= 0 &&
-		rect.left >= 0 &&
-		rect.bottom <=
-			(window.innerHeight || document.documentElement.clientHeight) &&
-		rect.right <=
-			(window.innerWidth || document.documentElement.clientWidth)
-	);
 }
 
 export function isValidUrl(string) {
