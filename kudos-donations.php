d16123a4dba81d50bfa4a123df8c47213d67d01c
michael <michael@iseard.media>
2023-10-03 11:53:54 +0200

    fix: remove return statement

--- kudos-donations.php
+++ kudos-donations.php
@@ -54,8 +54,8 @@ if ( file_exists( KUDOS_PLUGIN_DIR . '/vendor/woocommerce/action-scheduler/actio
 $dotenv = new Dotenv();
 try {
 	$dotenv->load( __DIR__ . '/.env' );
+	// phpcs:ignore
 } catch ( \Exception $ignored ) {
-	return [];
 }
 
 // Set the environment as production if not specified.
