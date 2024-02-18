8fdd1ffd0aa80660aeff960c8e40cba8b66f06f7
Michael Iseard <michael@iseard.media>
2024-02-19 12:57:13 +1300

    fix(admin): remove redundant code

--- includes/Controller/Admin.php
+++ includes/Controller/Admin.php
@@ -11,26 +11,21 @@ declare(strict_types=1);
 
 namespace IseardMedia\Kudos\Controller;
 
-use IseardMedia\Kudos\Helper\Assets;
 use IseardMedia\Kudos\Service\AbstractService;
 
 class Admin extends AbstractService {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public static function get_registration_actions(): array {
+		return [ 'admin_init' ];
+	}
+
 	/**
 	 * {@inheritDoc}
 	 */
 	public function register(): void {
-		add_action( 'admin_init', [ $this, 'register_block_editor_assets' ] );
-	}
-
-	/**
-	 * Register assets for enqueuing in the block editor.
-	 */
-	public function register_block_editor_assets(): void {
-		wp_register_style(
-			'kudos-donations-public',
-			Assets::get_style( 'admin/kudos-admin-campaigns.jsx.css' ),
-			[],
-			KUDOS_VERSION
-		);
+		// TODO add registration.
 	}
 }
