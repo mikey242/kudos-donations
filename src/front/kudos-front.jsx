bf5245e95744b001038007bb9a061b5274084e94
michael <michael@iseard.media>
2024-06-12 12:21:23 +0200

    fix(front): wrap code in listener function

--- src/front/kudos-front.jsx
+++ src/front/kudos-front.jsx
@@ -13,41 +13,43 @@ import CampaignProvider from '../contexts/CampaignContext';
 import './kudos-fonts.css';
 import './kudos-front.css';
 
-// Select the web components as target for render.
-const forms = document.querySelectorAll('.kudos-form');
-const messages = document.querySelectorAll('.kudos-message');
-const buttons = document.querySelectorAll('.button');
+document.addEventListener('DOMContentLoaded', function () {
+	// Select the web components as target for render.
+	const forms = document.querySelectorAll('.kudos-form');
+	const messages = document.querySelectorAll('.kudos-message');
+	const buttons = document.querySelectorAll('.kudos-button');
 
-// Kudos Donations buttons
-buttons.forEach((container) => {
-	const root = createRoot(container);
-	const campaignId = container.dataset.campaign;
-	const targetId = container.dataset.target;
-	const label = container.dataset.label;
-	root.render(
-		<CampaignProvider campaignId={campaignId}>
-			<KudosButton targetId={targetId} children={label} />
-		</CampaignProvider>
-	);
-});
+	// Kudos Donations buttons
+	buttons.forEach((container) => {
+		const root = createRoot(container);
+		const campaignId = container.dataset.campaign;
+		const targetId = container.dataset.target;
+		const label = container.dataset.label;
+		root.render(
+			<CampaignProvider campaignId={campaignId}>
+				<KudosButton targetId={targetId} children={label} />
+			</CampaignProvider>
+		);
+	});
 
-// Kudos Donations form/modal
-forms.forEach((container) => {
-	const root = createRoot(container);
-	const campaignId = container.dataset.campaign;
-	const displayAs = container.dataset.displayAs ?? 'button';
-	root.render(
-		<CampaignProvider campaignId={campaignId}>
-			<KudosForm displayAs={displayAs} />
-		</CampaignProvider>
-	);
-});
+	// Kudos Donations form/modal
+	forms.forEach((container) => {
+		const root = createRoot(container);
+		const campaignId = container.dataset.campaign;
+		const displayAs = container.dataset.displayAs ?? 'button';
+		root.render(
+			<CampaignProvider campaignId={campaignId}>
+				<KudosForm displayAs={displayAs} />
+			</CampaignProvider>
+		);
+	});
 
-// Kudos Donations message
-messages.forEach((container) => {
-	const root = createRoot(container);
-	const title = container.dataset.title;
-	const body = container.dataset.body;
-	const color = container.dataset.color;
-	root.render(<Message color={color} title={title} body={body} />);
+	// Kudos Donations message
+	messages.forEach((container) => {
+		const root = createRoot(container);
+		const title = container.dataset.title;
+		const body = container.dataset.body;
+		const color = container.dataset.color;
+		root.render(<Message color={color} title={title} body={body} />);
+	});
 });
