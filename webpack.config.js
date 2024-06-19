08605653e7675af962b77fb78e3040ac69e52d33
michael <michael@iseard.media>
2024-06-19 10:22:17 +0200

    fix(webpack): ensure translation functions are not mangled

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,20 @@
 const defaultConfig = require('@wordpress/scripts/config/webpack.config');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
 	...defaultConfig,
 	optimization: {
+		concatenateModules: false,
+		minimize: true,
+		minimizer: [
+			new TerserPlugin({
+				terserOptions: {
+					mangle: {
+						reserved: ['__', '_n', '_nx', '_x'],
+					},
+				},
+			}),
+		],
 		moduleIds: 'deterministic',
 		splitChunks: {
 			cacheGroups: {
