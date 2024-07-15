1a765ad395860b166735bb0d8ba8801fd84cc077
michael <michael@iseard.media>
2024-07-15 13:12:04 +0200

    fix(tests): Re-add functions line (oops)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -25,6 +25,9 @@ if ( ! file_exists( "{$_tests_dir}/includes/functions.php" ) ) {
 	exit( 1 );
 }
 
+// Give access to tests_add_filter() function.
+require_once "{$_tests_dir}/includes/functions.php";
+
 /**
  * Manually load the plugin being tested.
  */
