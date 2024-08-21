6a1ddb7ea589d123b6be89f9eba6f790124551f3
michael <michael@iseard.media>
2024-08-21 14:17:01 +0200

    fix(Tests): Update cache test with new static methods

--- tests/Service/CacheService.php
+++ tests/Service/CacheService.php
@@ -14,13 +14,6 @@ class CacheService extends WP_UnitTestCase {
 
 	public function setUp(): void {
 		/**
-		 * Set up cache service.
-		 */
-		$this->cache = new \IseardMedia\Kudos\Service\CacheService();
-		$logger      = $this->createMock( \Psr\Log\LoggerInterface::class );
-		$this->cache->setLogger( $logger );
-
-		/**
 		 * Create file ad folder in cache directory.
 		 */
 		$dir = KUDOS_CACHE_DIR . 'test/';
@@ -34,7 +27,7 @@ class CacheService extends WP_UnitTestCase {
 	 * Test that plugin is registered.
 	 */
 	public function test_purge_cache() {
-		$this->cache->purge_cache();
+		\IseardMedia\Kudos\Service\CacheService::recursively_clear_cache();
 		$this->assertSame( false, file_exists( $this->file ) );
 	}
 }
