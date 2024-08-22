bdc236207181b06ac389d7472e93fb6932ea2c99
michael <michael@iseard.media>
2024-08-22 11:41:19 +0200

    fix(Vendor): Clean up VendorInterface to remove unnecessary methods

--- includes/Vendor/VendorInterface.php
+++ includes/Vendor/VendorInterface.php
@@ -40,28 +40,6 @@ interface VendorInterface
 	public function verify_connection($data): WP_REST_Response;
 
     /**
-     * @param WP_Post $transaction ,
-     * @param string $mandate_id ,
-     * @param string $interval ,
-     * @param int $years
-     */
-    public function create_subscription(
-        WP_Post $transaction,
-        string $mandate_id,
-        string $interval,
-        int $years
-    );
-
-    /**
-     * Returns all subscriptions for customer.
-     *
-     * @param string $customer_id customer id.
-     *
-     * @since   2.0.0
-     */
-    public function get_subscriptions(string $customer_id);
-
-    /**
      * Cancel the specified subscription.
      *
      * @param WP_Post $subscription subscription row id.
@@ -69,16 +47,6 @@ interface VendorInterface
      * @return bool
      */
     public function cancel_subscription( WP_Post $subscription): bool;
-
-    /**
-     * Checks the provided api key by attempting to get associated payments.
-     *
-     * @param string $api_key API key to test.
-     *
-     * @return bool
-     * @since      1.0.0
-     */
-    public function refresh_api_connection(string $api_key): bool;
 
     /**
      * Create a customer.
@@ -89,22 +57,15 @@ interface VendorInterface
     public function create_customer(string $email, string $name);
 
     /**
-     * Get the customer.
-     *
-     * @param $vendor_customer_id
-     */
-    public function get_customer($vendor_customer_id);
-
-    /**
      * Creates a payment and returns it as an object.
      *
      * @param array $payment_args Parameters to pass to mollie to create a payment.
      * @param int $transaction_id Transaction ID.
      * @param string|null $vendor_customer_id ID of customer the payment is for.
      *
-     * @return string
+     * @return string|false
      */
-    public function create_payment(array $payment_args, int $transaction_id, ?string $vendor_customer_id): string;
+    public function create_payment(array $payment_args, int $transaction_id, ?string $vendor_customer_id);
 
 	/**
 	 * Refunds the provided transaction.
@@ -121,13 +82,6 @@ interface VendorInterface
      * @return WP_Error|WP_REST_Response
      */
     public function rest_webhook(WP_REST_Request $request);
-
-    /**
-     * Vendor API mode.
-     *
-     * @return string
-     */
-    public function get_api_mode(): string;
 
 	/**
 	 * Returns true is the API is ready to communicate.
