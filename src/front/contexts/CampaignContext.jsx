9cbe82252aa15aef14029fee39e7d719e0041637
michael <michael@iseard.media>
2024-09-16 13:06:34 +0200

    fix(front): revert to using apiFetch since useEntityRecords is intended for admin use

--- src/front/contexts/CampaignContext.jsx
+++ src/front/contexts/CampaignContext.jsx
@@ -1,8 +1,8 @@
 import React, { createContext, useContext } from 'react';
 import { __ } from '@wordpress/i18n';
-import { useEntityRecords } from '@wordpress/core-data';
 import { Flex, Spinner } from '@wordpress/components';
 import { useEffect, useMemo, useState } from '@wordpress/element';
+import apiFetch from '@wordpress/api-fetch';
 
 export const CampaignContext = createContext(null);
 
@@ -14,39 +14,30 @@ export default function CampaignProvider({ campaignId, children }) {
 	 * Fetch campaign by ID first, fallback to slug if not found.
 	 * Slug fetch is enabled only if the ID fetch resolves and finds nothing.
 	 */
-	const { records: campaignsById, hasResolved: hasResolvedById } =
-		useEntityRecords('postType', 'kudos_campaign', {
-			include: [campaignId],
-		});
-
-	const { records: campaignsBySlug, hasResolved: hasResolvedBySlug } =
-		useEntityRecords(
-			'postType',
-			'kudos_campaign',
-			{ slug: campaignId },
-			{
-				enabled:
-					hasResolvedById &&
-					(!campaignsById || !campaignsById.length),
-			}
-		);
-
 	useEffect(() => {
-		if (hasResolvedById && campaignsById?.length) {
-			setCampaign(campaignsById[0]);
-			setCampaignErrors(null); // Clear errors if campaign found by ID
-		} else if (hasResolvedBySlug && campaignsBySlug?.length) {
-			setCampaign(campaignsBySlug[0]);
-			setCampaignErrors(null); // Clear errors if campaign found by slug
-		} else if (
-			hasResolvedById &&
-			hasResolvedBySlug &&
-			!campaignsById?.length &&
-			!campaignsBySlug?.length
-		) {
-			setCampaignErrors([__('Campaign not found')]);
-		}
-	}, [campaignsById, campaignsBySlug, hasResolvedById, hasResolvedBySlug]);
+		// Fetch campaign by ID
+		apiFetch({
+			path: `/wp/v2/kudos_campaign/${campaignId}`,
+		})
+			.then((postById) => {
+				setCampaign(postById);
+			})
+			.catch((error) => {
+				// Most likely the post ID was not found. Try searching by slug instead.
+				if (error.data.status === 404) {
+					apiFetch({
+						path: `/wp/v2/kudos_campaign?slug=${campaignId}`,
+					}).then((postBySlug) => {
+						if (postBySlug.length > 0) {
+							setCampaign(postBySlug[0]);
+						} else {
+							// If neither ID nor slug finds a campaign, set the error.
+							setCampaignErrors([__('Campaign not found')]);
+						}
+					});
+				}
+			});
+	}, [campaignId]);
 
 	const contextValue = useMemo(
 		() => ({
