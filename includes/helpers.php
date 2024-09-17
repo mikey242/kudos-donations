9edf74c8e40a82a13c553f7b7da7c57f9b0663ca
michael <michael@iseard.media>
2024-09-17 14:38:45 +0200

    fix: check 'dd' is not already defined before declaring

--- includes/helpers.php
+++ includes/helpers.php
@@ -7,13 +7,15 @@
  * @copyright 2023 Iseard Media
  */
 
-/**
- * Dump provided parameter and stop.
- *
- * @param mixed $data Variable to dump.
- *  phpcs:disable WordPress.PHP.DevelopmentFunctions.error_log_print_r
- *  phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
- */
-function dd( $data ): void {
-	die( print( '<pre>' . print_r( $data, true ) . '</pre>' ) );
+if ( ! function_exists( 'dd' ) ) {
+	/**
+	 * Dump provided parameter and stop.
+	 *
+	 * @param mixed $data Variable to dump.
+	 *  phpcs:disable WordPress.PHP.DevelopmentFunctions.error_log_print_r
+	 *  phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
+	 */
+	function dd( $data ): void {
+		die( print( '<pre>' . print_r( $data, true ) . '</pre>' ) );
+	}
 }
