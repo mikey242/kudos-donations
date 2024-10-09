d8c780a9cdd2b466e6e7cb3bb1414c6787def8f6
michael <michael@iseard.media>
2024-10-09 10:43:23 +0200

    chore: bump minimum WordPress version to 6.6

--- kudos-donations.php
+++ kudos-donations.php
@@ -11,7 +11,7 @@
  * Version:           4.0.0-beta-4
  * Author:            Iseard Media
  * Author URI:        https://iseard.media
- * Requires at least: 6.2
+ * Requires at least: 6.6
  * Requires PHP:      7.4
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
