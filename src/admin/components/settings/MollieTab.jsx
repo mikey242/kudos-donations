d56e88454afb56242f95cc4824ea698aec502dca
michael <michael@iseard.media>
2024-11-07 09:58:16 +0100

    fix(admin): fix typo in payment methods description

--- src/admin/components/settings/MollieTab.jsx
+++ src/admin/components/settings/MollieTab.jsx
@@ -100,7 +100,7 @@ const MollieTab = () => {
 					)}
 					<p>
 						{__(
-							"These are the payment methods available to your donors. In order to use recurring payments (subscriptions) you will need to have either 'Card', 'PayPal' or 'SEPA Direct Debit'. Please note that 'SEPA Direct Debit' does not appear a payment option as it uses other payment options to set-up the subscription. If you have made changes to the payment methods in your Mollie dashboard, please click the refresh button below to update this list.",
+							"These are the payment methods available to your donors. In order to use recurring payments (subscriptions) you will need to have either 'Card', 'PayPal' or 'SEPA Direct Debit'. Please note that 'SEPA Direct Debit' does not appear as a payment option as it uses other payment options to set-up the subscription. If you have made changes to the payment methods in your Mollie dashboard, please click the refresh button below to update this list.",
 							'kudos-donations'
 						)}
 					</p>
