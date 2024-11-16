0991ca0532cf61181fe2530f09b476a0537b61bd
michael <michael@iseard.media>
2024-11-16 20:28:49 +0100

    fix(front): fix flicker on block editor when embedded form selected

--- src/front/components/FormRouter.jsx
+++ src/front/components/FormRouter.jsx
@@ -17,78 +17,39 @@ import {
 	LockClosedIcon,
 } from '@heroicons/react/24/outline';
 
-// Form router config.
-const steps = {
-	1: {
-		name: 'Initial',
-	},
-	2: {
-		name: 'Recurring',
-		requirements: {
-			recurring: true,
-		},
-	},
-	3: {
-		name: 'Address',
-		requirements: {
-			address_enabled: true,
-		},
-	},
-	4: {
-		name: 'Message',
-		requirements: {
-			message_enabled: true,
-		},
-	},
-	5: {
-		name: 'Summary',
-	},
-};
-
-export const FormRouter = ({
-	step,
-	campaign,
-	submitForm,
-	setFormState,
-	isPreview = false,
-}) => {
+export const FormRouter = ({ step, campaign, submitForm, setFormState }) => {
 	const [height, setHeight] = useState('');
-	const [currentStep, setCurrentStep] = useState(1);
+	const [currentStep, setCurrentStep] = useState(step);
+	const prevStep = useRef(step); // Ref to keep track of the previous step
 	const [isBusy, setIsBusy] = useState(false);
 	const elementRef = useRef(null);
+	const firstUpdate = useRef(true);
 	const methods = useForm({
 		defaultValues: {
-			recurring: false,
-			business_name: '',
-			city: '',
-			country: '',
-			postcode: '',
-			street: '',
-			message: '',
+			defaultValues: campaign.meta,
 		},
 	});
-	const firstUpdate = useRef(true);
 
 	const handlePrev = () => {
-		if (step === 1) {
+		if (currentStep === 1) {
 			return;
 		}
-		let prevStep = step - 1;
+		let prev = currentStep - 1;
 		const state = { ...methods.getValues(), ...campaign.meta };
 
 		// Find next available step.
-		while (!checkRequirements(state, prevStep) && prevStep >= 1) {
-			prevStep--;
+		while (!checkRequirements(state, prev) && prev >= 1) {
+			prev--;
 		}
-		setFormState((prev) => ({
-			...prev,
-			currentStep: prevStep,
+		setFormState((prevState) => ({
+			...prevState,
+			currentStep: prev,
 		}));
 	};
 
 	const handleNext = (data) => {
 		const state = { ...data, ...campaign.meta };
-		let nextStep = step + 1;
+		let nextStep = currentStep + 1;
 
 		// Find next available step.
 		while (!checkRequirements(state, nextStep) && nextStep <= 10) {
@@ -103,46 +64,56 @@ export const FormRouter = ({
 	};
 
 	const onSubmit = (data) => {
-		if (isPreview) {
-			return null;
-		}
-		if (step < 5) {
-			return handleNext(data, step + 1);
+		if (currentStep < 5) {
+			return handleNext(data, currentStep + 1);
 		}
 
 		setIsBusy(true);
 		submitForm(data).then((result) => {
-			if (!result.success) {
+			if (!result?.success) {
 				setIsBusy(false);
 			}
 		});
 	};
 
 	useLayoutEffect(() => {
+		// Skip on first render
 		if (firstUpdate.current) {
 			firstUpdate.current = false;
-		} else {
+			return;
+		}
+
+		// Only proceed if the step has changed
+		if (prevStep.current !== step) {
+			prevStep.current = step; // Update the previous step to the current step
+
 			if (!elementRef.current) {
 				return;
 			}
+
 			const target = elementRef.current;
 			target.classList.add('translate-x-1', 'opacity-0');
+
 			const oldHeight = target.querySelector('form').offsetHeight;
 			setHeight(oldHeight);
 			const resizeObserver = new ResizeObserver(() => {
 				const newHeight = target.querySelector('form').offsetHeight;
 				setHeight(newHeight);
+
 				const timeout = setTimeout(() => {
-					setHeight('auto'); // This allows form to grow if validation message appear.
+					setHeight('auto'); // Allow form to grow if validation message appears
 					setCurrentStep(step);
 					target.classList.remove('translate-x-1', 'opacity-0');
 				}, 200);
+
 				return () => clearTimeout(timeout);
 			});
+
 			resizeObserver.observe(target.querySelector('form'));
-			return () => resizeObserver.disconnect(); // clean up
+
+			return () => resizeObserver.disconnect();
 		}
-	}, [step]);
+	}, [step]); // Only rerun when `step` changes
 
 	return (
 		<FormProvider {...methods}>
@@ -156,68 +127,7 @@ export const FormRouter = ({
 				style={{ height: height + 'px' }}
 			>
 				<form id="kudos-form" onSubmit={methods.handleSubmit(onSubmit)}>
-					{
-						{
-							1: (
-								<InitialTab
-									title={campaign.meta.initial_title}
-									description={
-										campaign.meta.initial_description
-									}
-									currency={campaign.meta.currency}
-									minimumDonation={
-										campaign.meta.minimum_donation
-									}
-									maximumDonation={
-										campaign.meta.maximum_donation
-									}
-									donationType={campaign.meta.donation_type}
-									amountType={campaign.meta.amount_type}
-									fixedAmounts={campaign.meta.fixed_amounts}
-									showGoal={campaign.meta.show_goal}
-									goal={campaign.meta.goal}
-									total={campaign.total}
-									anonymous={campaign.meta.allow_anonymous}
-								/>
-							),
-							2: (
-								<FrequencyTab
-									title={campaign.meta.subscription_title}
-									description={
-										campaign.meta.subscription_description
-									}
-								/>
-							),
-							3: (
-								<AddressTab
-									required={campaign.meta.address_required}
-									title={campaign.meta.address_title}
-									description={
-										campaign.meta.address_description
-									}
-								/>
-							),
-							4: (
-								<MessageTab
-									title={campaign.meta.message_title}
-									description={
-										campaign.meta.message_description
-									}
-								/>
-							),
-							5: (
-								<SummaryTab
-									title={campaign.meta.payment_title}
-									description={
-										campaign.meta.payment_description
-									}
-									currency={campaign.meta.currency}
-									privacyLink={campaign.meta.privacy_link}
-									termsLink={campaign.meta.terms_link}
-								/>
-							),
-						}[currentStep]
-					}
+					<Tab step={currentStep} campaign={campaign} />
 					<div
 						id="kudos-form-buttons"
 						className="mt-8 flex justify-between relative"
@@ -260,6 +170,90 @@ export const FormRouter = ({
 			</div>
 		</FormProvider>
 	);
+};
+
+// Form router config.
+const steps = {
+	1: {
+		name: 'Initial',
+	},
+	2: {
+		name: 'Recurring',
+		requirements: {
+			recurring: true,
+		},
+	},
+	3: {
+		name: 'Address',
+		requirements: {
+			address_enabled: true,
+		},
+	},
+	4: {
+		name: 'Message',
+		requirements: {
+			message_enabled: true,
+		},
+	},
+	5: {
+		name: 'Summary',
+	},
+};
+
+const Tab = ({ step, campaign }) => {
+	switch (step) {
+		case 1:
+			return (
+				<InitialTab
+					title={campaign.meta.initial_title}
+					description={campaign.meta.initial_description}
+					currency={campaign.meta.currency}
+					minimumDonation={campaign.meta.minimum_donation}
+					maximumDonation={campaign.meta.maximum_donation}
+					donationType={campaign.meta.donation_type}
+					amountType={campaign.meta.amount_type}
+					fixedAmounts={campaign.meta.fixed_amounts}
+					showGoal={campaign.meta.show_goal}
+					goal={campaign.meta.goal}
+					total={campaign.total}
+					anonymous={campaign.meta.allow_anonymous}
+				/>
+			);
+		case 2:
+			return (
+				<FrequencyTab
+					title={campaign.meta.subscription_title}
+					description={campaign.meta.subscription_description}
+				/>
+			);
+		case 3:
+			return (
+				<AddressTab
+					required={campaign.meta.address_required}
+					title={campaign.meta.address_title}
+					description={campaign.meta.address_description}
+				/>
+			);
+		case 4:
+			return (
+				<MessageTab
+					title={campaign.meta.message_title}
+					description={campaign.meta.message_description}
+				/>
+			);
+		case 5:
+			return (
+				<SummaryTab
+					title={campaign.meta.payment_title}
+					description={campaign.meta.payment_description}
+					currency={campaign.meta.currency}
+					privacyLink={campaign.meta.privacy_link}
+					termsLink={campaign.meta.terms_link}
+				/>
+			);
+		default:
+			return null;
+	}
 };
 
 const checkRequirements = (data, target) => {
