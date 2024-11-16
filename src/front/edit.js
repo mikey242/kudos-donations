be40815d2ac7c0f5cdbd048416bf972e4aff0b19
michael <michael@iseard.media>
2024-11-16 17:15:56 +0100

    fix(block): simplify block edit code

--- src/front/edit.js
+++ src/front/edit.js
@@ -1,31 +1,22 @@
 /* eslint camelcase: 0 */
-
-import {
-	InspectorControls,
-	RichText,
-	useBlockProps,
-} from '@wordpress/block-editor';
+import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
 import {
 	ExternalLink,
-	Flex,
-	FlexItem,
 	PanelBody,
 	RadioControl,
 	SelectControl,
+	TextControl,
 } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import React from 'react';
-// eslint-disable-next-line import/default
-import { DonateButton } from './components/DonateButton';
-import { FormRouter } from './components/FormRouter';
-import Render from './components/Render';
 import { useCampaignContext } from './contexts/CampaignContext';
 import { useEntityRecords } from '@wordpress/core-data';
 import { useEffect } from '@wordpress/element';
+import { KudosForm } from './components/KudosForm';
 
 const ButtonEdit = (props) => {
 	const {
-		attributes: { button_label, campaign_id, type, className },
+		attributes: { button_label, campaign_id, type },
 		setAttributes,
 	} = props;
 	const { campaign: currentCampaign } = useCampaignContext();
@@ -53,121 +44,88 @@ const ButtonEdit = (props) => {
 	};
 
 	useEffect(() => {
-		if (campaigns?.length > 0) {
-			if (!campaign_id) {
-				setAttributes({ campaign_id: String(campaigns[0].id) });
-			}
+		if (!campaign_id && campaigns?.length > 0) {
+			setAttributes({ campaign_id: String(campaigns[0].id) });
 		}
 	}, [campaign_id, campaigns, setAttributes]);
 
 	return (
 		<div {...blockProps}>
 			{campaigns && (
-				<>
-					<InspectorControls>
-						<PanelBody
-							title={__('Campaign', 'kudos-donations')}
-							initialOpen={false}
-						>
-							<SelectControl
-								label={__(
-									'Select a campaign',
-									'kudos-donations'
-								)}
-								value={campaign_id}
-								onChange={onChangeCampaign}
-								options={[{ label: '', value: '' }].concat(
-									campaigns?.map((item) => ({
-										label: item?.title.rendered,
-										value: item.id,
-									}))
-								)}
+				<InspectorControls>
+					<PanelBody
+						title={__('Campaign Settings', 'kudos-donations')}
+						initialOpen={true}
+					>
+						<SelectControl
+							label={__('Select Campaign', 'kudos-donations')}
+							value={campaign_id}
+							onChange={onChangeCampaign}
+							options={
+								campaigns?.map((item) => ({
+									label: item?.title.rendered,
+									value: item.id,
+								})) || []
+							}
+							__nextHasNoMarginBottom
+						/>
+						{currentCampaign && (
+							<ExternalLink
+								href={`admin.php?page=kudos-campaigns&edit=${currentCampaign?.id}`}
+							>
+								{__('Edit', 'kudos-donations') +
+									' ' +
+									currentCampaign?.title?.rendered}
+							</ExternalLink>
+						)}
+					</PanelBody>
+					<PanelBody
+						title={__('Appearance', 'kudos-donations')}
+						initialOpen={false}
+					>
+						<RadioControl
+							label={__('Display Type', 'kudos-donations')}
+							selected={type}
+							options={[
+								{
+									label: __('Form', 'kudos-donations'),
+									value: 'form',
+								},
+								{
+									label: __(
+										'Button with Modal',
+										'kudos-donations'
+									),
+									value: 'button',
+								},
+							]}
+							onChange={onChangeType}
+						/>
+						{type === 'button' && (
+							<TextControl
+								label={__('Button Label', 'kudos-donations')}
+								value={button_label}
+								onChange={onChangeButtonLabel}
 								__nextHasNoMarginBottom
 							/>
-							<Flex>
-								<FlexItem>
-									{currentCampaign && (
-										<>
-											<ExternalLink
-												href={`admin.php?page=kudos-campaigns&edit=${currentCampaign?.id}`}
-											>
-												{__('Edit', 'kudos-donations') +
-													' ' +
-													currentCampaign?.title
-														?.rendered}
-											</ExternalLink>
-											<br />
-										</>
-									)}
-									<ExternalLink href="admin.php?page=kudos-campaigns&tab_name=campaigns">
-										{__(
-											'Create a new campaign',
-											'kudos-donations'
-										)}
-									</ExternalLink>
-								</FlexItem>
-							</Flex>
-						</PanelBody>
-
-						<PanelBody
-							title={__('Format', 'kudos-donations')}
-							initialOpen={false}
-						>
-							<RadioControl
-								label={__('Display as', 'kudos-donations')}
-								selected={type}
-								options={[
-									{
-										label: __(
-											'Button with modal',
-											'kudos-donations'
-										),
-										value: 'button',
-									},
-									{
-										label: __(
-											'Embedded form',
-											'kudos-donations'
-										),
-										value: 'form',
-									},
-								]}
-								onChange={onChangeType}
-							/>
-						</PanelBody>
-					</InspectorControls>
-					<>
-						{currentCampaign ? (
-							<Render
-								className={className ?? ''}
-								style={currentCampaign?.meta?.custom_styles}
-								themeColor={currentCampaign?.meta?.theme_color}
-							>
-								{type === 'form' ? (
-									<FormRouter
-										isPreview={true}
-										campaign={currentCampaign}
-									/>
-								) : (
-									<DonateButton>
-										<RichText
-											allowedFormats={[]} // Disable all formatting
-											onChange={onChangeButtonLabel}
-											value={button_label}
-										/>
-									</DonateButton>
-								)}
-							</Render>
-						) : (
-							<p>
-								Kudos Donations:{' '}
-								{__(
-									'Please select a campaign from the sidebar to continue.'
-								)}
-							</p>
 						)}
-					</>
-				</>
+					</PanelBody>
+				</InspectorControls>
+			)}
+			{currentCampaign ? (
+				<KudosForm
+					displayAs={type}
+					label={button_label}
+					preventSubmit={true}
+				/>
+			) : (
+				<p>
+					Kudos Donations:{' '}
+					{__(
+						'Please select a campaign from the sidebar to continue.',
+						'kudos-donations'
+					)}
+				</p>
 			)}
 		</div>
 	);
