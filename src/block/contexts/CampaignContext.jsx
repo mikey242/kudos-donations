2a064f708901ec728ea7723ea73d666914846c68
michael <michael@iseard.media>
2024-11-19 12:52:29 +0100

    fix(admin): improve handling of null or empty campaign_id prop

--- src/block/contexts/CampaignContext.jsx
+++ src/block/contexts/CampaignContext.jsx
@@ -9,34 +9,44 @@ export const CampaignContext = createContext(null);
 export default function CampaignProvider({ campaignId, children }) {
 	const [campaign, setCampaign] = useState(null);
 	const [campaignErrors, setCampaignErrors] = useState(null);
+	const [isLoading, setIsLoading] = useState(false);
 
 	/**
 	 * Fetch campaign by ID first, fallback to slug if not found.
 	 * Slug fetch is enabled only if the ID fetch resolves and finds nothing.
 	 */
 	useEffect(() => {
-		// Fetch campaign by ID
-		apiFetch({
-			path: `/wp/v2/kudos_campaign/${campaignId}`,
-		})
-			.then((postById) => {
-				setCampaign(postById);
+		if (campaignId) {
+			setCampaignErrors(null);
+			setIsLoading(true);
+			// Fetch campaign by ID
+			apiFetch({
+				path: `/wp/v2/kudos_campaign/${campaignId}`,
 			})
-			.catch((error) => {
-				// Most likely the post ID was not found. Try searching by slug instead.
-				if (error?.data?.status === 404) {
-					apiFetch({
-						path: `/wp/v2/kudos_campaign?slug=${campaignId}`,
-					}).then((postBySlug) => {
-						if (postBySlug.length > 0) {
-							setCampaign(postBySlug[0]);
-						} else {
-							// If neither ID nor slug finds a campaign, set the error.
-							setCampaignErrors([__('Campaign not found')]);
-						}
-					});
-				}
-			});
+				.then((postById) => {
+					setCampaign(postById);
+				})
+				.catch((error) => {
+					// Most likely the post ID was not found. Try searching by slug instead.
+					if (error?.data?.status === 404) {
+						apiFetch({
+							path: `/wp/v2/kudos_campaign?slug=${campaignId}`,
+						}).then((postBySlug) => {
+							if (postBySlug.length > 0) {
+								setCampaign(postBySlug[0]);
+							} else {
+								// If neither ID nor slug finds a campaign, set the error.
+								setCampaignErrors([
+									__('Campaign not found', 'kudos-donations'),
+								]);
+							}
+						});
+					}
+				})
+				.finally(() => {
+					setIsLoading(false);
+				});
+		}
 	}, [campaignId]);
 
 	const contextValue = useMemo(
@@ -47,7 +57,7 @@ export default function CampaignProvider({ campaignId, children }) {
 		[campaign, campaignErrors]
 	);
 
-	if (!campaign && !campaignErrors) {
+	if (isLoading) {
 		return (
 			<Flex justify="center">
 				<Spinner />
