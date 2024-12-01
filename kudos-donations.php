9b95ffac7b208e76dd0d6b6beaf426f17439e390
michael <michael@iseard.media>
2024-12-01 12:55:59 +0100

    chore: bump version to 4.0.0-beta-13

--- kudos-donations.php
+++ kudos-donations.php
@@ -8,7 +8,7 @@
  * Plugin Name:       Kudos Donations
  * Plugin URI:        https://gitlab.iseard.media/michael/kudos-donations
  * Description:       Add a donation button to any page on your website. Easy & fast setup. Works with Mollie payments.
- * Version:           4.0.0-beta-12
+ * Version:           4.0.0-beta-13
  * Author:            Iseard Media
  * Author URI:        https://iseard.media
  * Requires at least: 6.6
@@ -17,8 +17,6 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       kudos-donations
  * Domain Path:       /languages
- *
- * @fs_premium_only /premium
  */
 
 namespace IseardMedia\Kudos;
@@ -33,7 +31,7 @@ if ( ! \defined( 'WPINC' ) ) {
 /**
  * Define all the Kudos Donations constants for use throughout the plugin.
  */
-\define( 'KUDOS_VERSION', '4.0.0-beta-12' );
+\define( 'KUDOS_VERSION', '4.0.0-beta-13' );
 \define( 'KUDOS_DB_VERSION', '4.0.0' );
 \define( 'KUDOS_PLUGIN_FILE', __FILE__ );
 \define( 'KUDOS_PLUGIN_URL', plugin_dir_url( KUDOS_PLUGIN_FILE ) );
