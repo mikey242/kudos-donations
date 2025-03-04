30aeec3c01acd06d35e39372648c49f12a9e38e2
Michael Iseard <michael@iseard.media>
2025-03-04 11:57:35 +0100

    fix(lint): fix linting issues

--- src/admin/components/controls/TextControl.jsx
+++ src/admin/components/controls/TextControl.jsx
@@ -7,53 +7,60 @@ import {
 	// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
 	__experimentalInputControlSuffixWrapper as InputControlSuffixWrapper,
 } from '@wordpress/components';
+import { forwardRef } from '@wordpress/element';
 import { BaseControl } from './BaseControl';
 
-export const TextControl = ({
-	name,
-	rules,
-	isDisabled,
-	label,
-	help,
-	suffix,
-	prefix,
-	type = 'text',
-	placeholder,
-}) => {
-	return (
-		<BaseControl
-			name={name}
-			rules={rules}
-			isDisabled={isDisabled}
-			help={help}
-			render={({ onChange, value, description }) => (
-				<>
-					<InputControl
-						label={label}
-						value={value ?? ''}
-						disabled={isDisabled}
-						onChange={onChange}
-						help={description}
-						placeholder={placeholder}
-						prefix={
-							prefix && (
-								<InputControlPrefixWrapper>
-									{prefix}
-								</InputControlPrefixWrapper>
-							)
-						}
-						type={type}
-						suffix={
-							suffix && (
-								<InputControlSuffixWrapper>
-									{suffix}
-								</InputControlSuffixWrapper>
-							)
-						}
-						__next40pxDefaultSize
-					/>
-				</>
-			)}
-		/>
-	);
-};
+export const TextControl = forwardRef(
+	(
+		{
+			name,
+			rules,
+			isDisabled,
+			label,
+			help,
+			suffix,
+			prefix,
+			type = 'text',
+			placeholder,
+		},
+		ref
+	) => {
+		return (
+			<BaseControl
+				name={name}
+				rules={rules}
+				isDisabled={isDisabled}
+				help={help}
+				render={({ onChange, value, description }) => (
+					<>
+						<InputControl
+							ref={ref}
+							label={label}
+							value={value ?? ''}
+							disabled={isDisabled}
+							onChange={onChange}
+							help={description}
+							placeholder={placeholder}
+							prefix={
+								prefix && (
+									<InputControlPrefixWrapper>
+										{prefix}
+									</InputControlPrefixWrapper>
+								)
+							}
+							type={type}
+							suffix={
+								suffix && (
+									<InputControlSuffixWrapper>
+										{suffix}
+									</InputControlSuffixWrapper>
+								)
+							}
+							__next40pxDefaultSize
+						/>
+					</>
+				)}
+			/>
+		);
+	}
+);
