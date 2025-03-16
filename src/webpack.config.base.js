4c67e44edba2e688fd739a8889ff49bf3f4c876b
Michael Iseard <michael@iseard.media>
2025-03-16 15:01:16 +0100

    fix(dev): remove redundant optimization config

--- src/webpack.config.base.js
+++ src/webpack.config.base.js
@@ -1,30 +1,9 @@
 const defaultConfig = require('@wordpress/scripts/config/webpack.config');
-const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
 	...defaultConfig,
 	optimization: {
-		concatenateModules: false,
-		minimize: true,
-		minimizer: [
-			new TerserPlugin({
-				terserOptions: {
-					mangle: {
-						reserved: ['__', '_n', '_nx', '_x'],
-					},
-					compress: {
-						passes: 2, // Fixes issues with gettext for some reason.
-					},
-					format: {
-						comments(node, comment) {
-							const text = comment.value;
-							return /(translators:)/i.test(text);
-						},
-					},
-				},
-				extractComments: false,
-			}),
-		],
+		...defaultConfig.optimization,
 		moduleIds: 'deterministic',
 	},
 };
