62e85ed91f5f3f96e9c924d443abdf691b8f36ff
Michael Iseard <michael@iseard.media>
2025-03-25 12:16:52 +0100

    fix(infra): Re-add custom rest field for returning donor for transaction

--- includes/Domain/PostType/TransactionPostType.php
+++ includes/Domain/PostType/TransactionPostType.php
@@ -13,12 +13,13 @@ namespace IseardMedia\Kudos\Domain\PostType;
 
 use IseardMedia\Kudos\Domain\HasAdminColumns;
 use IseardMedia\Kudos\Domain\HasMetaFieldsInterface;
+use IseardMedia\Kudos\Domain\HasRestFieldsInterface;
 use IseardMedia\Kudos\Enum\FieldType;
 use IseardMedia\Kudos\Enum\PaymentStatus;
 use IseardMedia\Kudos\Helper\Utils;
 use IseardMedia\Kudos\Vendor\PaymentVendor\MolliePaymentVendor;
 
-class TransactionPostType extends AbstractCustomPostType implements HasMetaFieldsInterface, HasAdminColumns {
+class TransactionPostType extends AbstractCustomPostType implements HasMetaFieldsInterface, HasRestFieldsInterface, HasAdminColumns {
 
 	/**
 	 * Meta field constants.
@@ -39,6 +40,7 @@ class TransactionPostType extends AbstractCustomPostType implements HasMetaField
 	public const META_FIELD_VENDOR_SUBSCRIPTION_ID = 'vendor_subscription_id';
 	public const META_FIELD_INVOICE_NUMBER         = 'invoice_number';
 	public const META_FIELD_CHECKOUT_URL           = 'checkout_url';
+	public const REST_FIELD_DONOR                  = 'donor';
 
 	/**
 	 * {@inheritDoc}
@@ -272,6 +274,20 @@ class TransactionPostType extends AbstractCustomPostType implements HasMetaField
 			self::META_FIELD_MESSAGE     => [
 				'value_type' => FieldType::STRING,
 				'label'      => __( 'Message', 'kudos-donations' ),
+			],
+		];
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function get_rest_fields(): array {
+		return [
+			self::REST_FIELD_DONOR => [
+				'get_callback' => function ( $transaction ) {
+					$donor_id = $transaction['meta'][ self::META_FIELD_DONOR_ID ] ?? null;
+					return DonorPostType::get_post_using_rest( $donor_id );
+				},
 			],
 		];
 	}
