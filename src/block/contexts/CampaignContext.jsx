c68ae3e0c9afd8ce670e299a128201abebf3c3fe
michael <michael@iseard.media>
2025-04-24 15:55:30 +0200

    fix(front): Fix FOUC when using fetching by campaign slug

--- src/block/contexts/CampaignContext.jsx
+++ src/block/contexts/CampaignContext.jsx
@@ -10,7 +10,6 @@ export default function CampaignProvider({ campaignId, children }) {
 	const [campaign, setCampaign] = useState(null);
 	const [campaignErrors, setCampaignErrors] = useState(null);
 	const [isLoading, setIsLoading] = useState(false);
-
 	/**
 	 * Fetch campaign by ID first, fallback to slug if not found.
 	 * Slug fetch is enabled only if the ID fetch resolves and finds nothing.
@@ -19,32 +18,48 @@ export default function CampaignProvider({ campaignId, children }) {
 		if (campaignId) {
 			setCampaignErrors(null);
 			setIsLoading(true);
-			// Fetch campaign by ID
-			apiFetch({
-				path: `/wp/v2/kudos_campaign/${campaignId}`,
-			})
+
+			// Try by ID
+			apiFetch({ path: `/wp/v2/kudos_campaign/${campaignId}` })
 				.then((postById) => {
 					setCampaign(postById);
+					setIsLoading(false);
 				})
 				.catch((error) => {
-					// Most likely the post ID was not found. Try searching by slug instead.
 					if (error?.data?.status === 404) {
+						// Try by slug
 						apiFetch({
 							path: `/wp/v2/kudos_campaign?slug=${campaignId}`,
-						}).then((postBySlug) => {
-							if (postBySlug.length > 0) {
-								setCampaign(postBySlug[0]);
-							} else {
-								// If neither ID nor slug finds a campaign, set the error.
+						})
+							.then((postBySlug) => {
+								if (postBySlug.length > 0) {
+									setCampaign(postBySlug[0]);
+								} else {
+									setCampaignErrors([
+										__(
+											'Campaign not found',
+											'kudos-donations'
+										),
+									]);
+								}
+							})
+							.catch(() => {
 								setCampaignErrors([
-									__('Campaign not found', 'kudos-donations'),
+									__(
+										'Failed to fetch campaign by slug',
+										'kudos-donations'
+									),
 								]);
-							}
-						});
+							})
+							.finally(() => {
+								setIsLoading(false);
+							});
+					} else {
+						setCampaignErrors([
+							__('Failed to fetch campaign', 'kudos-donations'),
+						]);
+						setIsLoading(false);
 					}
-				})
-				.finally(() => {
-					setIsLoading(false);
 				});
 		}
 	}, [campaignId]);
