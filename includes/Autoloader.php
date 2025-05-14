3a7ccd6bee0e1c63ae30b861459d9e0b14b9b895
michael <michael@iseard.media>
2025-05-14 11:34:16 +0200

    fix(infra): Re-add missing autoloader

--- includes/Autoloader.php
+++ includes/Autoloader.php
@@ -26,6 +26,7 @@ class Autoloader {
 	public static function init(): bool {
 		$autoloaders = [
 			\dirname( __DIR__ ) . '/vendor/autoload_packages.php',
+			\dirname( __DIR__ ) . '/third-party/vendor/autoload.php',
 		];
 
 		// Load standard Composer autoloader.
