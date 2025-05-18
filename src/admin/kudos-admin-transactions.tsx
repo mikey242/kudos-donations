9e2273270e25a9a649802dd1ee73a25bcc6f87da
michael <michael@iseard.media>
2025-05-18 15:08:42 +0200

    feat(admin): Add Transactions admin table

---
+++ src/admin/kudos-admin-transactions.tsx
@@ -0,0 +1,24 @@
+import { createRoot } from '@wordpress/element';
+import React from 'react';
+import domReady from '@wordpress/dom-ready';
+import { AdminProvider, PostsProvider } from './components';
+import type { Campaign } from '../types/wp';
+import { __ } from '@wordpress/i18n';
+import { TransactionsTable } from './components/transactions/TransactionsTable';
+
+domReady(() => {
+	const container = document.getElementById('root');
+	if (container) {
+		const root = createRoot(container);
+		root.render(
+			<AdminProvider>
+				<PostsProvider<Campaign>
+					postType="kudos_transaction"
+					singular={__('Transaction', 'kudos-donations')}
+				>
+					<TransactionsTable />
+				</PostsProvider>
+			</AdminProvider>
+		);
+	}
+});
