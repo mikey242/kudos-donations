cfaedac6d00b28c86b8330ae063a3c926a056b7c
michael <michael@iseard.media>
2025-05-20 00:30:24 +0200

    fix(infra): Use instance of for class check and add rest query modifications

--- includes/Domain/PostType/AbstractCustomPostType.php
+++ includes/Domain/PostType/AbstractCustomPostType.php
@@ -60,19 +60,74 @@ abstract class AbstractCustomPostType extends AbstractContentType implements Cus
 	}
 
 	/**
+	 * Add meta to REST query.
+	 */
+	private function add_meta_to_rest_query() {
+		add_filter(
+			'rest_' . $this->get_slug() . '_collection_params',
+			function ( $params ) {
+				$params['orderby']['enum'][] = 'meta_value';
+				$params['orderby']['enum'][] = 'meta_value_num';
+				return $params;
+			}
+		);
+		add_filter(
+			'rest_' . $this->get_slug() . '_query',
+			function ( $args, $request ) {
+				$meta_key = $request->get_param( 'metaKey' );
+				if ( $meta_key ) {
+					$args['meta_key']   = sanitize_key( $meta_key );
+					$args['meta_value'] = sanitize_text_field( $request->get_param( 'metaValue' ) );
+
+					$meta_type = $request->get_param( 'metaType' );
+					if ( $meta_type ) {
+						$args['meta_type'] = sanitize_key( $meta_type );
+					}
+
+					$meta_compare      = strtoupper( $request->get_param( 'metaCompare' ) ?? '=' );
+					$valid_comparisons = [
+						'=',
+						'!=',
+						'>',
+						'>=',
+						'<',
+						'<=',
+						'LIKE',
+						'NOT LIKE',
+						'IN',
+						'NOT IN',
+						'EXISTS',
+						'NOT EXISTS',
+					];
+
+					if ( \in_array( $meta_compare, $valid_comparisons, true ) ) {
+						$args['meta_compare'] = $meta_compare;
+					}
+				}
+				return $args;
+			},
+			10,
+			2
+		);
+	}
+
+	/**
 	 * {@inheritDoc}
 	 */
 	public function register(): void {
-		$this->register_post_type();
-		if ( is_a( $this, HasMetaFieldsInterface::class ) ) {
+		if ( ! post_type_exists( $this->get_slug() ) ) {
+			$this->register_post_type();
+		}
+		if ( $this instanceof HasMetaFieldsInterface ) {
+			$this->add_meta_to_rest_query();
 			$this->register_meta_fields( apply_filters( $this->get_slug() . '_meta_fields', $this->get_meta_config() ), ObjectType::POST, $this->get_slug() );
 		}
 
-		if ( is_a( $this, HasRestFieldsInterface::class ) ) {
+		if ( $this instanceof HasRestFieldsInterface ) {
 			$this->register_rest_fields( apply_filters( $this->get_slug() . '_rest_fields', $this->get_rest_fields() ), $this->get_slug() );
 		}
 
-		if ( is_a( $this, HasAdminColumns::class ) ) {
+		if ( $this instanceof HasAdminColumns ) {
 			$this->add_table_columns( $this->get_slug(), $this->get_columns_config() );
 		}
 	}
