178cb388c3c8d23d5c55424bf93df6995820ec1d
michael <michael@iseard.media>
2025-07-21 13:16:51 +0200

    Add versioning to EncryptionService and rename *_password methods to be more generic

--- includes/Container/EncryptionAwareTrait.php
+++ includes/Container/EncryptionAwareTrait.php
@@ -56,7 +56,7 @@ trait EncryptionAwareTrait {
 		$num_asterisks = substr_count( $value, '*' );
 		$count         = \strlen( $value );
 		if ( $num_asterisks !== $count ) {
-			$encrypted_key = $this->get_encryption()->encrypt_password( $value );
+			$encrypted_key = $this->get_encryption()->encrypt( $value );
 			$stars         = str_repeat( '*', \strlen( $value ) );
 			$result        = update_option( $encrypted_option, $encrypted_key );
 			if ( $result && \is_callable( $callback ) ) {
@@ -75,7 +75,7 @@ trait EncryptionAwareTrait {
 	 */
 	protected function get_decrypted_key( string $encrypted_option ): string {
 		$encrypted_key = get_option( $encrypted_option, '' );
-		$decrypted     = $this->get_encryption()->decrypt_password( $encrypted_key );
+		$decrypted     = $this->get_encryption()->decrypt( $encrypted_key );
 
 		// If salt check failed or decryption failed.
 		if ( false === $decrypted ) {
