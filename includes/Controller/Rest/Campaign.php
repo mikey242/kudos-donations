6eb9a366d2df12d98d869777557103455e185b62
michael <michael@iseard.media>
2026-01-27 20:59:34 +1300

    Create and implement a by-slug route for campaigns with public accessibility

--- includes/Controller/Rest/Campaign.php
+++ includes/Controller/Rest/Campaign.php
@@ -18,6 +18,7 @@ use IseardMedia\Kudos\Domain\Repository\CampaignRepository;
 use WP_Error;
 use WP_REST_Request;
 use WP_REST_Response;
+use WP_REST_Server;
 
 /**
  * @extends BaseRepositoryRestController<CampaignEntity>
@@ -40,6 +41,44 @@ class Campaign extends BaseRepositoryRestController {
 	}
 
 	/**
+	 * {@inheritDoc}
+	 */
+	protected function get_additional_routes(): array {
+		return [
+			'/by-slug/(?P<slug>[a-z0-9-]+)' => [
+				[
+					'methods'             => WP_REST_Server::READABLE,
+					'callback'            => [ $this, 'get_item_by_slug' ],
+					'permission_callback' => $this->can_read_one(),
+					'args'                => [
+						'slug' => [
+							'required' => true,
+							'type'     => 'string',
+						],
+					],
+				],
+			],
+		];
+	}
+
+	/**
+	 * Get a single campaign by its slug.
+	 *
+	 * @param WP_REST_Request $request The request object.
+	 * @return WP_Error|WP_REST_Response
+	 */
+	public function get_item_by_slug( WP_REST_Request $request ) {
+		$slug = $request->get_param( 'slug' );
+		$item = $this->repository->find_one_by( [ 'wp_post_slug' => $slug ] );
+
+		if ( ! $item ) {
+			return new WP_Error( 'not_found', __( 'Campaign not found.', 'kudos-donations' ), [ 'status' => 404 ] );
+		}
+
+		return new WP_REST_Response( $this->add_rest_fields( $item ), 200 );
+	}
+
+	/**
 	 * Get a single entity by ID.
 	 *
 	 * @param WP_REST_Request $request The request object.
@@ -48,13 +87,12 @@ class Campaign extends BaseRepositoryRestController {
 	public function get_item( WP_REST_Request $request ) {
 		$id = (int) $request->get_param( 'id' );
 
-		// Try to find the entity by id and if none found assume it is the wp_post_id or wp_post_slug from earlier versions.
+		// Try to find the entity by id and if none found assume it is the wp_post_id from earlier versions.
 		$item = $this->repository->get( $id ) ??
-				$this->repository->find_one_by( [ 'wp_post_id' => $id ] ) ??
-				$this->repository->find_one_by( [ 'wp_post_slug' => $id ] );
+				$this->repository->find_one_by( [ 'wp_post_id' => $id ] );
 
 		if ( ! $item ) {
-			// translators: %s is the entity type singular name (e.g Transaction).
+			// translators: %s is the entity type singular name (e.g. Transaction).
 			return new WP_Error( 'not_found', \sprintf( __( '%s not found.', 'kudos-donations' ), $this->repository::get_singular_name() ), [ 'status' => 404 ] );
 		}
 
