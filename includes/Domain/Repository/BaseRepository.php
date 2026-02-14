ec90997f50fc7129c33cd3a81e0901880b5166bc
michael <michael@iseard.media>
2026-02-14 18:19:02 +1300

    Make BaseEntity->id nullable

--- includes/Domain/Repository/BaseRepository.php
+++ includes/Domain/Repository/BaseRepository.php
@@ -178,7 +178,7 @@ abstract class BaseRepository implements RepositoryInterface, RepositoryAwareInt
 	 * @return int|false The inserted or updated row ID, or false on failure.
 	 */
 	public function upsert( BaseEntity $entity ) {
-		if ( $entity->id ?? false ) {
+		if ( isset( $entity->id ) ) {
 			$result = $this->update( $entity ) ? $entity->id : false;
 		} else {
 			$result = $this->insert( $entity );
