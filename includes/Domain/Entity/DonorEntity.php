c40b8134ed0fb48fddf153cba1a805f52305ab97
michael <michael@iseard.media>
2026-02-14 20:13:41 +1300

    Remove null initialization values from nullable entity properties

--- includes/Domain/Entity/DonorEntity.php
+++ includes/Domain/Entity/DonorEntity.php
@@ -14,14 +14,14 @@ namespace IseardMedia\Kudos\Domain\Entity;
 class DonorEntity extends BaseEntity {
 
 	public string $email;
-	public ?string $mode               = null;
-	public ?string $name               = null;
-	public ?string $business_name      = null;
-	public ?string $street             = null;
-	public ?string $postcode           = null;
-	public ?string $city               = null;
-	public ?string $country            = null;
-	public ?string $locale             = null;
-	public ?string $vendor_customer_id = null;
-	public ?float $total               = null;
+	public ?string $mode;
+	public ?string $name;
+	public ?string $business_name;
+	public ?string $street;
+	public ?string $postcode;
+	public ?string $city;
+	public ?string $country;
+	public ?string $locale;
+	public ?string $vendor_customer_id;
+	public ?float $total;
 }
