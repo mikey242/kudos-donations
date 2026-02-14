f0f38a1a03556f0a717782853f92da3d3fe9dbfc
michael <michael@iseard.media>
2026-02-14 19:36:48 +1300

    Add initial values for $title, $created_at and $updated_at entity properties

--- includes/Domain/Entity/BaseEntity.php
+++ includes/Domain/Entity/BaseEntity.php
@@ -13,11 +13,11 @@ namespace IseardMedia\Kudos\Domain\Entity;
 
 class BaseEntity {
 
-	public ?int $id = null;
-	public string $title;
+	public ?int $id      = null;
+	public string $title = '';
 	public ?int $wp_post_id;
-	public string $created_at;
-	public string $updated_at;
+	public string $created_at = '';
+	public string $updated_at = '';
 
 	/**
 	 * Constructor for assigning object properties.
