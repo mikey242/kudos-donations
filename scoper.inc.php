ed699cee8aa9b5e4acf02d85b2f5b76530d64aea
michael <michael@iseard.media>
2026-02-16 17:36:09 +1300

    Add Dompdf scoping

--- scoper.inc.php
+++ scoper.inc.php
@@ -10,8 +10,11 @@ $finders = [
 	      ->ignoreVCS(true)
 	      ->ignoreDotFiles(true)
 	      ->path([
+			  'dompdf/',
+			  'masterminds/',
 			  'mollie/',
-		      'monolog/',
+			  'monolog/',
+			  'sabberworm/',
 	      ])
 	      ->in('vendor'),
 ];
@@ -22,17 +25,34 @@ return [
 	'exclude-namespaces' => [
 		'IseardMedia\\Kudos\\',
 		'Psr\\',
-		'Composer',
-		'Masterminds'
+		'Composer'
 	],
 	'patchers' => [
 		static function (string $filePath, string $prefix, string $content): string {
-			// Rewrite legacy Mollie docblocks to match scoped namespace
-			return preg_replace_callback(
+			// Rewrite legacy Mollie docblocks to match scoped namespace.
+			$content = preg_replace_callback(
 				'/@return\s+\\\\Mollie\\\\Api\\\\Resources\\\\([A-Za-z]+)/',
 				static fn(array $matches): string => '@return \\' . $prefix . '\\Mollie\\Api\\Resources\\' . $matches[1],
 				$content
 			);
+
+			// Rewrite dynamic Dompdf class name strings.
+			$escaped_prefix = str_replace('\\', '\\\\', $prefix);
+			$content = str_replace(
+				'"Dompdf\\\\FrameDecorator\\\\',
+				'"' . $escaped_prefix . '\\\\Dompdf\\\\FrameDecorator\\\\',
+				$content
+			);
+			$content = str_replace(
+				'"Dompdf\\\\FrameReflower\\\\',
+				'"' . $escaped_prefix . '\\\\Dompdf\\\\FrameReflower\\\\',
+				$content
+			);
+            return str_replace(
+                "'\\Dompdf\\Positioner\\\\",
+                "'\\" . $prefix . "\\Dompdf\\Positioner\\\\",
+                $content
+            );
 		},
 	],
 ];
