2a23619dd25355b2301bc0a5cece0a1e612c091b
michael <michael@iseard.media>
2026-02-17 18:53:35 +1300

    Add sanitize callback for country

--- includes/Domain/Schema/DonorSchema.php
+++ includes/Domain/Schema/DonorSchema.php
@@ -12,6 +12,7 @@ declare(strict_types=1);
 namespace IseardMedia\Kudos\Domain\Schema;
 
 use IseardMedia\Kudos\Enum\FieldType;
+use IseardMedia\Kudos\Helper\Country;
 
 class DonorSchema extends BaseSchema {
 
@@ -27,9 +28,29 @@ class DonorSchema extends BaseSchema {
 			'street'             => $this->make_schema_field( FieldType::STRING, 'sanitize_text_field' ),
 			'postcode'           => $this->make_schema_field( FieldType::STRING, 'sanitize_text_field' ),
 			'city'               => $this->make_schema_field( FieldType::STRING, 'sanitize_text_field' ),
-			'country'            => $this->make_schema_field( FieldType::STRING, 'sanitize_text_field' ),
+			'country'            => $this->make_schema_field( FieldType::STRING, [ $this, 'sanitize_country' ] ),
 			'locale'             => $this->make_schema_field( FieldType::STRING, 'sanitize_text_field' ),
 			'vendor_customer_id' => $this->make_schema_field( FieldType::STRING, 'sanitize_text_field' ),
 		];
+	}
+
+	/**
+	 * Sanitise country input.
+	 *
+	 * Accepts ISO 3166-1 alpha-2 codes or full country names.
+	 * Full names are converted to alpha-2 codes where possible.
+	 *
+	 * @param string $value The country value to sanitise.
+	 */
+	public function sanitize_country( string $value ): string {
+		$value = sanitize_text_field( $value );
+
+		// Already a 2-letter code.
+		if ( preg_match( '/^[A-Z]{2}$/i', $value ) ) {
+			return strtoupper( $value );
+		}
+
+		// Attempt to convert full name to ISO code.
+		return Country::get_iso_code_by_name( $value ) ?? '';
 	}
 }
