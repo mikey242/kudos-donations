4200124a825c5e95b81f6e971e17b5bb954f5160
michael <michael@iseard.media>
2026-03-02 13:45:56 +1300

    Remove useState for selected tab and only rely on URL

--- src/admin/components/AdminTabPanel.tsx
+++ src/admin/components/AdminTabPanel.tsx
@@ -4,7 +4,6 @@ import {
 	// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
 	__experimentalVStack as VStack,
 } from '@wordpress/components';
-import { useState } from '@wordpress/element';
 import { useAdminQueryParams } from '../hooks';
 
 export interface AdminTab {
@@ -21,21 +20,12 @@ export const AdminTabPanel = ({
 	tabs,
 }: AdminTabPanelProps): React.ReactNode => {
 	const { params, updateParams } = useAdminQueryParams();
-	const initialTab = params.tab ?? tabs[0]?.name;
-	const [selectedTab, setSelectedTab] = useState(initialTab);
-
-	const updateTab = (tab: string) => {
-		if (tab !== selectedTab) {
-			setSelectedTab(tab);
-			void updateParams({ tab });
-		}
-	};
 
 	return (
 		<div className="kudos-settings-tab-panel">
 			<TabPanel
-				initialTabName={selectedTab}
-				onSelect={updateTab}
+				initialTabName={params.tab || tabs[0]?.name}
+				onSelect={(tab) => void updateParams({ tab })}
 				tabs={tabs}
 			>
 				{(tab) => <VStack spacing={4}>{tab.content}</VStack>}
