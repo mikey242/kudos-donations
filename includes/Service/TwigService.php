e161a277d0947a6a31e9ab74934ea67508763a73
michael <michael@iseard.media>
2026-03-04 19:14:42 +1300

    Scope Twig

--- includes/Service/TwigService.php
+++ includes/Service/TwigService.php
@@ -12,14 +12,14 @@ namespace IseardMedia\Kudos\Service;
 use IseardMedia\Kudos\Container\ActivationAwareInterface;
 use IseardMedia\Kudos\Helper\Assets;
 use IseardMedia\Kudos\Helper\Utils;
+use IseardMedia\Kudos\ThirdParty\Twig\Environment;
+use IseardMedia\Kudos\ThirdParty\Twig\Extension\DebugExtension;
+use IseardMedia\Kudos\ThirdParty\Twig\Loader\FilesystemLoader;
+use IseardMedia\Kudos\ThirdParty\Twig\TwigFilter;
+use IseardMedia\Kudos\ThirdParty\Twig\TwigFunction;
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerAwareTrait;
 use Throwable;
-use Twig\Environment;
-use Twig\Extension\DebugExtension;
-use Twig\Loader\FilesystemLoader;
-use Twig\TwigFilter;
-use Twig\TwigFunction;
 
 class TwigService implements ActivationAwareInterface, LoggerAwareInterface {
 
@@ -33,7 +33,7 @@ class TwigService implements ActivationAwareInterface, LoggerAwareInterface {
 	 * Twig constructor
 	 */
 	public function __construct() {
-		$this->options['cache'] = self::CACHE_DIR;
+		$this->options['cache'] = self::CACHE_DIR . KUDOS_VERSION . '/';
 		$this->options['debug'] = KUDOS_DEBUG;
 
 		$this->twig = new Environment(
