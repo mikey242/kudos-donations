30429cc7ceba74b3c37b588a53242973233b2205
michael <michael@iseard.media>
2026-03-05 21:15:21 +1300

    Expose admin controls and panel in windows.kudos.admin object

--- src/admin/kudos-admin.tsx
+++ src/admin/kudos-admin.tsx
@@ -3,7 +3,15 @@ import React from 'react';
 import domReady from '@wordpress/dom-ready';
 import './kudos-admin.css';
 import { AdminProvider, SettingsProvider } from './contexts';
-import { AdminRouter } from './components';
+import { AdminRouter, Panel } from './components';
+import * as Controls from './components/controls';
+
+window.kudos.admin = {
+	Controls,
+	Components: {
+		Panel,
+	},
+};
 
 domReady(() => {
 	const container = document.getElementById('root');
