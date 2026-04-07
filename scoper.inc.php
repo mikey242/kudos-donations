8c3e1f7d89d9d4e403809e435be15e8e959765bc
michael <michael@iseard.media>
2026-04-07 12:05:51 +0200

    Add link to scoper config documentation and specify php version

--- scoper.inc.php
+++ scoper.inc.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
  * php-scoper entry point. Merges all per-package configs from config/php-scoper/.
  * Each *.inc.php file returns an array with 'finders' and 'patchers' keys.
  *
- * @see config/php-scoper/
+ * @see https://github.com/humbug/php-scoper/blob/main/docs/configuration.md
  */
 
 $configs  = array_map( fn( $file ) => require $file, glob( __DIR__ . '/config/php-scoper/*.inc.php' ) ?: [] );
@@ -15,6 +15,7 @@ $patchers = ! empty( $configs ) ? array_merge( ...array_column( $configs, 'patch
 
 return [
 	'prefix'                  => 'IseardMedia\\Kudos\\ThirdParty',
+	'php-version'             => '7.4',
 	'finders'                 => $finders,
 	'patchers'                => $patchers,
 	'exclude-namespaces'      => [
