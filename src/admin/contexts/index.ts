0fd96cf34f9f380df38aab169796fe5fb8066ce1
michael <michael@iseard.media>
2026-05-22 11:30:24 +0200

    Re-add hooks to context export

--- src/admin/contexts/index.ts
+++ src/admin/contexts/index.ts
@@ -2,3 +2,4 @@ export * from './admin-context';
 export * from './entities-context';
 export * from './settings-context';
 export { useFormContext, useWatch } from 'react-hook-form';
+export { useOnSettingsSaved, usePageTitle } from '../hooks';
