55c89f0ec52a946a13e41e73f955516681901239
michael <michael@iseard.media>
2026-05-22 11:10:26 +0200

    Don't export hooks from context

--- src/admin/contexts/index.ts
+++ src/admin/contexts/index.ts
@@ -2,4 +2,3 @@ export * from './admin-context';
 export * from './entities-context';
 export * from './settings-context';
 export { useFormContext, useWatch } from 'react-hook-form';
-export { useOnSettingsSaved, usePageTitle } from '../hooks';
