3e8eb00d710af3b9c1be0ceab209bc68879437be
michael <michael@iseard.media>
2026-05-25 15:04:38 +0200

    Temporarily prevent payment icons from being processed by CopyWebpackPlugin

--- src/admin/webpack.config.admin.js
+++ src/admin/webpack.config.admin.js
@@ -12,7 +12,7 @@ module.exports = merge(commonConfig, {
 		new CopyWebpackPlugin({
 			patterns: [
 				{
-					from: resolve(__dirname, '../images'),
+					from: resolve(__dirname, '../images/logo-colour.png'),
 					to: 'images',
 				},
 			],
