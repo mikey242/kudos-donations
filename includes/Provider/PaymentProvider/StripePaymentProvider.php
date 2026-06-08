1f871f4509ad366e7b59f6c5747298f3c0ad7588
michael <michael@iseard.media>
2026-06-08 13:22:48 +0200

    Fix specification of incorrect number of hook arguments

--- includes/Provider/PaymentProvider/StripePaymentProvider.php
+++ includes/Provider/PaymentProvider/StripePaymentProvider.php
@@ -107,8 +107,8 @@ class StripePaymentProvider extends AbstractPaymentProvider {
 		add_action( 'kudos_stripe_handle_status_change', [ $this, 'handle_status_change' ] );
 		add_filter( 'pre_update_option_' . self::SETTING_API_KEY_LIVE, [ $this, 'handle_key_update' ], 10, 3 );
 		add_filter( 'pre_update_option_' . self::SETTING_API_KEY_TEST, [ $this, 'handle_key_update' ], 10, 3 );
-		add_action( 'update_option_' . self::SETTING_API_KEY_ENCRYPTED_LIVE, [ $this, 'handle_key_updated' ], 10, 3 );
-		add_action( 'update_option_' . self::SETTING_API_KEY_ENCRYPTED_TEST, [ $this, 'handle_key_updated' ], 10, 3 );
+		add_action( 'update_option_' . self::SETTING_API_KEY_ENCRYPTED_LIVE, [ $this, 'handle_key_updated' ], 10, 2 );
+		add_action( 'update_option_' . self::SETTING_API_KEY_ENCRYPTED_TEST, [ $this, 'handle_key_updated' ], 10, 2 );
 	}
 
 	/**
