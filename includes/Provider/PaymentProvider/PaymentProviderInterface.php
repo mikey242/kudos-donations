70dff5abf6d1d6115f5f543e568fb83689485a42
michael <michael@iseard.email>
2026-07-21 12:23:49 +0200

    Remove unused import

--- includes/Provider/PaymentProvider/PaymentProviderInterface.php
+++ includes/Provider/PaymentProvider/PaymentProviderInterface.php
@@ -11,7 +11,6 @@ namespace IseardMedia\Kudos\Provider\PaymentProvider;
 
 use IseardMedia\Kudos\Domain\Entity\SubscriptionEntity;
 use IseardMedia\Kudos\Domain\Entity\TransactionEntity;
-use IseardMedia\Kudos\Notice\Notice;
 use IseardMedia\Kudos\Provider\ProviderInterface;
 use WP_REST_Request;
 use WP_REST_Response;
